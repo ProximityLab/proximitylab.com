2e407a09bdee9f59c46c10d2844771eb653b7d0f
Nuno Arruda <nuno@nunoarruda.com>
2017-07-19 16:01:49 +0200

    feat: add Nye jumbotron

--- includes/nye.php
+++ includes/nye.php
@@ -1,1 +1,17 @@
-nye
+<div class="jumbotron jumbotron-fluid project-jumbotron diagonal-grad-2">
+    
+    <?php include('includes/header.php'); ?>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-md-4 project-jumbotron-padding">
+                <h1 class="pl-heading-xl pl-color-white mt-5 pt-5">explore the world outside</h1>
+                <img class="project-logo d-block mx-auto my-4 my-xl-5" src="images/logo-nye.png" alt="Nye logo">
+                <p class="pl-text-lg pl-color-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor feugiat posuere. Vestibulum finibus neque vitae leo semper porttitor eu vel sapien. Donec sollicitudin augue</p>
+                <img class="icon-arrow-bottom mx-auto d-block" src="images/icon-arrow-bottom.svg" alt="Bottom arrow icon">
+            </div>
+            <div class="col-12 col-md-8 project-jumbotron-padding"><img class="img-fluid mt-5" src="images/website-mockup-nye.png" alt="Nye website mockup"></div>
+        </div>
+    </div>
+
+</div>
