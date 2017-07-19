23ddfe335929f667a68b968a7c598a99020977cc
Nuno Arruda <nuno@nunoarruda.com>
2017-07-19 13:12:42 +0200

    refactor: rename connect-sync gulp task to serve

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
     connect = require('gulp-connect-php'),
     browserSync = require('browser-sync');
 
-gulp.task('connect-sync', function() {
+gulp.task('serve', function() {
     connect.server({}, function(){
         browserSync({
             proxy: '127.0.0.1:8000'
