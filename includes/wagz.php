de27f4770d1796a1153e32e1d3fa72e5ebdcb8d9
Nuno Arruda <nuno@nunoarruda.com>
2017-07-19 13:45:25 +0200

    refactor: move wagz content to a php include

---
+++ includes/wagz.php
@@ -0,0 +1,79 @@
+<div class="jumbotron jumbotron-fluid project-jumbotron diagonal-grad-1">
+    <img class="pl-logo" src="images/logo-proximity-lab-white.svg" alt="Proximity Lab logo">
+    <img class="icon-grid-view" src="images/icon-grid-view.svg" alt="Grid view icon">
+    <img class="icon-arrow-left" src="images/icon-arrow-left.svg" alt="Left arrow icon">
+    <img class="icon-arrow-right" src="images/icon-arrow-right.svg" alt="Right arrow icon">
+    
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-md-4 project-jumbotron-padding">
+                <h1 class="pl-heading-xl pl-color-white pl-heading-jumbotron">keep fido safe from the world</h1>
+                <p class="pl-text-lg pl-color-white">Lorem ipsum dolor sit amet, consectetur</p>
+                <img class="icon-arrow-bottom mx-auto d-block" src="images/icon-arrow-bottom.svg" alt="Bottom arrow icon">
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 project-jumbotron-padding"><img class="img-fluid" src="images/iphone-mockup-wagz.png" alt="Wagz iPhone Mockup"></div>
+            <div class="col-12 col-sm-6 col-md-4 align-self-center project-jumbotron-padding">
+                <img class="project-logo" src="images/logo-wagz.png" alt="Wagz logo">
+                <p class="pl-text-sm pl-color-white">Lorem ipsum dolor sit amet, consectetur</p>
+            </div>
+        </div><!-- row -->
+    </div><!-- container -->
+</div><!-- jumbotron -->
+
+<section class="service-info">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-md-6 align-self-center">
+                <div class="service-info-text">
+                    <h4 class="pl-heading-sm pl-color-boulder service-info-heading1">PORTFOLIO</h4>
+                    <h2 class="pl-heading-lg pl-color-mineshaft service-info-heading2">Creating an easier way to project your best companion</h2>
+                    <p class="pl-text-lg pl-color-boulder service-info-paragraph">Our team can support your initiatives by developing design concepts, detailed interface designs and templates or by producing complete solutions from concept and design through development, testing and deployment.</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 carousel-placeholder"></div>
+        </div>
+    </div>
+</section>
+
+<section class="project-video">
+    <img class="img-fluid" src="images/video-background-wagz.jpg" alt="Wagz video background">
+    <img class="project-video-play" src="images/icon-play.svg" alt="Play icon" data-toggle="modal" data-target="#video-modal">
+</section>
+
+<!-- Modal -->
+<div class="modal fade" id="video-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe id="video-iframe" class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 col-sm-4 align-self-center text-center">
+            <div class="how-info">
+                <img class="how-circle" src="images/icon-circle.svg" alt="Circle icon">
+                <h3 class="pl-heading-md pl-color-tundora how-heading">Designing</h3>
+                <p class="pl-text-md pl-color-boulder how-text">Our team can support your initiatives by developing design concepts, detailed interface designs and templates or by</p>
+            </div>
+        </div>
+        <div class="col-12 col-sm-4"><img class="img-fluid how-dog" src="images/wagz-dog.png" alt="Wagz dog"></div>
+        <div class="col-12 col-sm-4 align-self-center text-center">
+            <div class="how-info">
+                <img class="how-circle" src="images/icon-circle.svg" alt="Circle icon">
+                <h3 class="pl-heading-md pl-color-tundora how-heading">Designing</h3>
+                <p class="pl-text-md pl-color-boulder how-text">Our team can support your initiatives by developing design concepts, detailed interface designs and templates or by</p>
+            </div>
+        </div>
+    </div>
+</div>
