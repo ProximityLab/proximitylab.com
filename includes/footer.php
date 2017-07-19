496def7a172ed470fd865a25c7eff5a89bdb063d
Nuno Arruda <nuno@nunoarruda.com>
2017-07-19 13:34:33 +0200

    refactor: move footer to a php include

---
+++ includes/footer.php
@@ -0,0 +1,38 @@
+<footer class="page-footer">
+    <img class="pl-logo" src="images/logo-proximity-lab-pink.svg" alt="Proximity Lab logo">
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-lg-5">
+                <h1 class="pl-heading-xl pl-color-white page-footer-heading">let us help change your experience</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-lg-5">
+                <p class="pl-text-lg pl-color-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor feugiat posuere. Vestibulum finibus neque vitae leo semper porttitor eu vel sapien. Donec sollicitudin augue sed nulla scelerisque, id tempor tellus molestie. Ut elementum feugiat dolor, in vestibulum velit gravida at.</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                <a href="#" class="footer-button">Let’s Talk</a>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                <ul class="footer-links-container">
+                <li class="footer-item"><a class="footer-link" href="">Home</a></li>
+                <li class="footer-item"><a class="footer-link" href="">About</a></li>
+                <li class="footer-item"><a class="footer-link" href="">Work</a></li>
+                <li class="footer-item"><a class="footer-link" href="">Contact</a></li> 
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <p class="pl-text-xs pl-color-white">© 2008-2017 Proximity Lab, All Rights Reserved</p>
+            </div>
+        </div>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -89,44 +89,7 @@
             </div>
         </div>
 
-        <footer class="page-footer">
-            <img class="pl-logo" src="images/logo-proximity-lab-pink.svg" alt="Proximity Lab logo">
-
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-12 col-lg-5">
-                        <h1 class="pl-heading-xl pl-color-white page-footer-heading">let us help change your experience</h1>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12 col-lg-5">
-                        <p class="pl-text-lg pl-color-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor feugiat posuere. Vestibulum finibus neque vitae leo semper porttitor eu vel sapien. Donec sollicitudin augue sed nulla scelerisque, id tempor tellus molestie. Ut elementum feugiat dolor, in vestibulum velit gravida at.</p>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-12">
-                        <a href="#" class="footer-button">Let’s Talk</a>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-12">
-                        <ul class="footer-links-container">
-                        <li class="footer-item"><a class="footer-link" href="">Home</a></li>
-                        <li class="footer-item"><a class="footer-link" href="">About</a></li>
-                        <li class="footer-item"><a class="footer-link" href="">Work</a></li>
-                        <li class="footer-item"><a class="footer-link" href="">Contact</a></li> 
-                        </ul>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12">
-                        <p class="pl-text-xs pl-color-white">© 2008-2017 Proximity Lab, All Rights Reserved</p>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        <?php include('includes/footer.php'); ?>
 
         <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
