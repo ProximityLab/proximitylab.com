f342673b18b4ff5bae12022ba600ba3022a7f8f1
Nuno Arruda <nuno@nunoarruda.com>
2017-07-21 15:31:25 +0200

    refactor: make video autoplay logic host independent

--- src/custom.js
+++ src/custom.js
@@ -1,8 +1,12 @@
 // Video autoplay on modal
-$('#video-modal').on('shown.bs.modal', function (e) {
-    $('#video-iframe').attr('src', 'https://www.youtube.com/embed/zpOULjyy-n8?rel=0&autoplay=1');
+var videoModal = $('#video-modal');
+var videoIframe = $('#video-iframe');
+var videoSrc = videoIframe.attr('src');
+
+videoModal.on('shown.bs.modal', function (e) {
+    videoIframe.attr('src', videoSrc + '&autoplay=1');
 });
 
-$('#video-modal').on('hide.bs.modal', function (e) {
-    $('#video-iframe').attr('src', 'https://www.youtube.com/embed/zpOULjyy-n8?rel=0');
+videoModal.on('hide.bs.modal', function (e) {
+    videoIframe.attr('src', videoSrc);
 });
