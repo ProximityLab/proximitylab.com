a07170e384bc592942904fbad521dd9e01d0f3ad
Nuno Arruda <nuno@nunoarruda.com>
2017-07-21 16:36:32 +0200

    feat: add amadeus jumbotron

--- src/includes/amadeus.php
+++ src/includes/amadeus.php
@@ -1,1 +1,17 @@
-amadeus
+<div class="jumbotron jumbotron-fluid project-jumbotron diagonal-grad-3">
+
+    <?php include('includes/header.php'); ?>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-md-4 project-jumbotron-padding">
+                <h1 class="pl-heading-xl pl-color-white mt-5 pt-5">plan your space</h1>
+                <img class="project-logo project-logo-amadeus my-4 my-xl-5" src="images/logo-amadeus.png" alt="Amadeus logo">
+                <p class="pl-text-lg pl-color-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor feugiat posuere. Vestibulum finibus neque vitae leo semper porttitor eu vel sapien. Donec sollicitudin augue</p>
+                <img class="icon-arrow-bottom mx-auto d-block" src="images/icon-arrow-bottom.svg" alt="Bottom arrow icon">
+            </div>
+            <div class="col-12 col-md-8 project-jumbotron-padding"><img class="img-fluid mt-5" src="images/website-mockup-amadeus.png" alt="Amadeus website mockup"></div>
+        </div>
+    </div>
+
+</div>
