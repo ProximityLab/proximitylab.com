fcefc358879301595273f9a3477382345504c396
Nuno Arruda <nuno@nunoarruda.com>
2017-07-25 14:30:29 +0200

    feat: add dropdown menu

--- src/includes/header.php
+++ src/includes/header.php
@@ -1,4 +1,17 @@
-<img class="pl-logo" src="images/logo-proximity-lab-white.svg" alt="Proximity Lab logo">
+
 <img class="icon-grid-view" src="images/icon-grid-view.svg" alt="Grid view icon">
 <img class="icon-arrow-left" src="images/icon-arrow-left.svg" alt="Left arrow icon">
 <img class="icon-arrow-right" src="images/icon-arrow-right.svg" alt="Right arrow icon">
+
+<div class="dropdown">
+  <img class="dropdown-logo" src="images/logo-proximity-lab-white.svg" alt="Proximity Lab logo">
+  <div class="dropdown-content">
+    <a class="pl-text-md dropdown-link" href="#">Home</a>
+    <hr class="dropdown-horizontal-rule">
+    <a class="pl-text-md dropdown-link" href="#">About</a>
+    <hr class="dropdown-horizontal-rule">
+    <a class="pl-text-md dropdown-link" href="#">Work</a>
+    <hr class="dropdown-horizontal-rule">
+    <a class="pl-text-md dropdown-link" href="#">Contact</a>
+  </div>
+</div>
