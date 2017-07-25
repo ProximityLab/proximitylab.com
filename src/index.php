8fc6e53b416212d1d892b4fd8833c526ec3ec634
Nuno Arruda <nuno@nunoarruda.com>
2017-07-25 14:53:26 +0200

    feat: animate body tag

--- src/index.php
+++ src/index.php
@@ -9,7 +9,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" integrity="sha384-OHBBOqpYHNsIqQy8hL1U+8OXf9hH6QRxi0+EODezv82DfnZoV7qoHAZDwMwEJvSw" crossorigin="anonymous">
         <link rel="stylesheet" href="custom.css">
     </head>
-    <body>
+    <body class="animated fadeIn">
         <?php
             switch ($_GET['project']) {
                 case 'nye':
