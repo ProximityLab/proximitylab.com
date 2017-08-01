0c8bab3810b797864943c6992eb77ea2a70d1007
Nuno Arruda <nuno@nunoarruda.com>
2017-08-01 15:07:27 +0200

    feat: add hero images carousel in homepage header

--- src/includes/home.php
+++ src/includes/home.php
@@ -1,1 +1,7 @@
-test
+<div id="carouselHome" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner" role="listbox">
+    <div class="carousel-item carousel-item1 active"></div>
+    <div class="carousel-item carousel-item1"></div>
+    <div class="carousel-item carousel-item1"></div>
+  </div>
+</div>
