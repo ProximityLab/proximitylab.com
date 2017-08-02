34d6d44816897d40050061d21c56cfe813d14d41
Nuno Arruda <nuno@nunoarruda.com>
2017-08-02 14:56:36 +0200

    feat: add onScroll animated logo on homepage hero header

--- src/custom.js
+++ src/custom.js
@@ -10,3 +10,15 @@ videoModal.on('shown.bs.modal', function (e) {
 videoModal.on('hide.bs.modal', function (e) {
     videoIframe.attr('src', videoSrc);
 });
+
+
+// homepage hero logo animation on scroll
+var logo = $('#pl-logo-hero');
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 5) {
+        logo.addClass('pl-logo-hero-animate');
+    } else {
+        logo.removeClass('pl-logo-hero-animate');
+    }
+});
--- src/includes/home.php
+++ src/includes/home.php
@@ -5,6 +5,8 @@
     <a class="pl-nav-link" href="#">Work</a>
   </nav>
 
+  <a href="/"><img id="pl-logo-hero" class="pl-logo-hero" src="images/logo-proximity-lab-white.svg" alt="Proximity Lab logo"></a>
+
   <div class="carousel-inner" role="listbox">
     <div class="carousel-item carousel-item1 active"></div>
     <div class="carousel-item carousel-item1"></div>
