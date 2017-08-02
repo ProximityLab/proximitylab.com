dc4e962c7f84fbf4d9c93f209d58095644da4349
Nuno Arruda <nuno@nunoarruda.com>
2017-08-02 14:55:24 +0200

    feat: add new navigation to homepage

--- src/includes/home.php
+++ src/includes/home.php
@@ -1,4 +1,10 @@
 <div id="carouselHome" class="carousel slide" data-ride="carousel">
+
+  <nav class="pl-nav">
+    <a class="pl-nav-link" href="#">About</a>
+    <a class="pl-nav-link" href="#">Work</a>
+  </nav>
+
   <div class="carousel-inner" role="listbox">
     <div class="carousel-item carousel-item1 active"></div>
     <div class="carousel-item carousel-item1"></div>
