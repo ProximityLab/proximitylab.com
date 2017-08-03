0a5ffff8cd929476aa2668886d5a593245a34706
Nuno Arruda <nuno@nunoarruda.com>
2017-08-03 13:53:13 +0200

    feat: add carousel text

--- src/includes/home.php
+++ src/includes/home.php
@@ -8,8 +8,20 @@
   <a href="/"><img id="pl-logo-hero" class="pl-logo-hero" src="images/logo-proximity-lab-white.svg" alt="Proximity Lab logo"></a>
 
   <div class="carousel-inner" role="listbox">
-    <div class="carousel-item carousel-item1 active"></div>
-    <div class="carousel-item carousel-item1"></div>
-    <div class="carousel-item carousel-item1"></div>
+    <div class="carousel-item carousel-item1 active">
+      <div class="carousel-caption d-none d-md-block">
+        <p class="carousel-text">Proximity Lab is an award-winning interaction design studio with a 25-year track record providing product strategy, user</p>
+      </div>
+    </div>
+    <div class="carousel-item carousel-item1">
+      <div class="carousel-caption d-none d-md-block">
+        <p class="carousel-text">Proximity Lab is an award-winning interaction design studio with a 25-year track record providing product strategy, user</p>
+      </div>
+    </div>
+    <div class="carousel-item carousel-item1">
+      <div class="carousel-caption d-none d-md-block">
+        <p class="carousel-text">Proximity Lab is an award-winning interaction design studio with a 25-year track record providing product strategy, user</p>
+      </div>
+    </div>
   </div>
 </div>
