420618fe882158602599711a15859883d882b2af
Nuno Arruda <nuno@nunoarruda.com>
2017-08-09 21:58:43 +0200

    feat: add about page story section

--- src/includes/pages/about.php
+++ src/includes/pages/about.php
@@ -14,3 +14,16 @@
 
     <p class="about-header-text text-center">Proximity Lab is an award-winning interaction design studio with a 25-year track record providing product strategy, user experience design and front-end development to trusted brands and startups.</p>
 </header>
+
+<div class="container-fluid about-service">
+    <div class="row">
+        <div class="col-12 col-md-7 about-service-spacing">
+            <h4 class="about-service-heading-secondary">ABOUT PROXIMITY LAB</h4>
+            <h3 class="about-service-heading-primary">The Proximity Lab story</h3>
+            <p class="about-service-text">Our team can support your initiatives by developing design concepts, detailed interface designs and templates or by producing complete solutions from concept and design through development, testing and deployment.</p>
+            <p class="about-service-text">Our team can support your initiatives by developing design concepts, detailed interface designs and templates or by producing complete solutions from concept and design through development, testing and deployment.</p>
+        </div>
+
+        <div class="col-12 col-md-5 about-service-image about-service-image-size"></div>
+    </div>
+</div>
