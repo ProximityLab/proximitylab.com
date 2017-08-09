a222a316d708c01af5159be000d7d5ecaeaad1d8
Nuno Arruda <nuno@nunoarruda.com>
2017-08-09 17:23:50 +0200

    feat: add navigation on about page

--- src/includes/pages/about.php
+++ src/includes/pages/about.php
@@ -1,3 +1,8 @@
 <header class="about-header">
-    test
+    <a href="/"><img class="about-logo" src="images/logo-proximity-lab-white.svg" alt="Proximity Lab logo"></a>
+
+    <nav class="about-nav">
+        <a class="about-nav-link about-nav-link-active" href="?page=about">About</a>
+        <a class="about-nav-link" href="#">Work</a>
+    </nav>
 </header>
