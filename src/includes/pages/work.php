8a863b1f81e1de3bed4e491329db4ce93f48c970
Nuno Arruda <nuno@nunoarruda.com>
2017-08-11 18:38:01 +0200

    feat: add work page header

--- src/includes/pages/work.php
+++ src/includes/pages/work.php
@@ -1,1 +1,12 @@
-test
+<header class="work-header">
+    <a href="/"><img class="work-logo" src="images/logo-proximity-lab-pink.svg" alt="Proximity Lab logo"></a>
+
+    <nav class="work-nav">
+        <a class="work-nav-link" href="?page=about">About</a>
+        <a class="work-nav-link work-nav-link-active" href="?page=work">Work</a>
+    </nav>
+
+    <h1 class="work-header-heading text-center">The fruits of our labor</h1>
+
+    <p class="work-header-text text-center mx-auto">Our team can support your initiatives by developing design concepts, detailed interface designs and templates or by producing complete solutions from</p>
+</header>
