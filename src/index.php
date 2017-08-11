958f11ebecb28931e43484b8748336923ecce360
Nuno Arruda <nuno@nunoarruda.com>
2017-08-11 17:51:40 +0200

    feat: create work page

--- src/index.php
+++ src/index.php
@@ -12,12 +12,16 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" integrity="sha384-OHBBOqpYHNsIqQy8hL1U+8OXf9hH6QRxi0+EODezv82DfnZoV7qoHAZDwMwEJvSw" crossorigin="anonymous">
         <link rel="stylesheet" href="css/main.css">
         <?php if ($activePage == 'about') echo '<link rel="stylesheet" href="css/about.css">'; ?>
+        <?php if ($activePage == 'work') echo '<link rel="stylesheet" href="css/work.css">'; ?>
     </head>
     <body class="animated fadeIn">
         <?php
             switch ($activePage) {
                 case 'about':
                     include('includes/pages/about.php');
+                    break;
+                case 'work':
+                    include('includes/pages/work.php');
                     break;
                 default:
                     include('includes/pages/home.php');
