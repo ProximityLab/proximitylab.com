032f69fc0ba37c3c0a181ce9fecff38c7c95ab6b
Nuno Arruda <nuno@nunoarruda.com>
2017-08-14 17:52:23 +0200

    refactor: modularize page footer

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -1,29 +1,29 @@
 <footer class="page-footer">
-    <img class="pl-logo" src="images/logo-proximity-lab-pink.svg" alt="Proximity Lab logo">
+    <img class="page-footer-logo" src="images/logo-proximity-lab-pink.svg" alt="Proximity Lab logo">
 
     <div class="container-fluid">
         <div class="row mb-5">
             <div class="col-12 col-md-8">
-                <h1 class="pl-heading-lg pl-color-white page-footer-heading">Let us help change your experience</h1>
+                <h1 class="page-footer-heading">Let us help change your experience</h1>
             </div>
 
             <div class="col-12 col-md-4 mt-5 mt-md-4 mt-xl-3">
-                <a href="#" class="footer-button">Get in Touch</a>
+                <a href="#" class="page-footer-button">Get in Touch</a>
             </div>
         </div>
 
         <div class="row">
             <div class="col-12">
-                <ul class="footer-links-container">
-                    <li class="footer-item"><a class="footer-link" href="">Home</a></li>
-                    <li class="footer-item"><a class="footer-link" href="">About</a></li>
-                    <li class="footer-item"><a class="footer-link" href="">Work</a></li>
+                <ul class="page-footer-links-container">
+                    <li class="page-footer-item"><a class="page-footer-link" href="/">Home</a></li>
+                    <li class="page-footer-item"><a class="page-footer-link" href="?page=about">About</a></li>
+                    <li class="page-footer-item"><a class="page-footer-link" href="?page=work">Work</a></li>
                 </ul>
             </div>
         </div>
         <div class="row">
             <div class="col-12">
-                <p class="pl-text-sm pl-color-dustygrey">© 2008-2017 Proximity Lab, All Rights Reserved <span class="footer-separator">|</span> 855.776.9522 <span class="footer-separator">|</span> info@proximitylab.com</p>
+                <p class="page-footer-credits">© 2008-2017 Proximity Lab, All Rights Reserved <span class="page-footer-separator">|</span> 855.776.9522 <span class="page-footer-separator">|</span> info@proximitylab.com</p>
             </div>
         </div>
     </div>
