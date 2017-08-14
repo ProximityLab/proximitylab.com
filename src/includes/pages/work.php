5b84940108861c13afc3f75fb23bbeb95b297a52
Nuno Arruda <nuno@nunoarruda.com>
2017-08-14 16:18:24 +0200

    feat: add work page nye content

--- src/includes/pages/work.php
+++ src/includes/pages/work.php
@@ -13,7 +13,13 @@
 
 <div class="container-fluid">
     <div class="row">
-        <div class="work-item col-12 col-md-8"><div class="work-item-container work-item-nye"></div></div>
+        <div class="work-item col-12 col-md-8">
+            <div class="work-item-container work-item-nye text-center">
+                <img class="work-item-logo work-item-logo-nye" src="images/logo-nye.png" alt="Nye logo">
+                <p class="work-item-text mx-auto">Our team can support your initiatives by developing design concepts, detailed interface designs and templates or by producing complete</p>
+                <a class="work-item-button" href="">View work</a>
+            </div>
+        </div>
         <div class="work-item col-12 col-sm-6 col-md-4"><div class="work-item-container work-item-wagz"></div></div>
         <div class="work-item col-12 col-sm-6 col-md-4"><div class="work-item-container work-item-amadeus"></div></div>
         <div class="work-item col-12 col-md-8"><div class="work-item-container work-item-fpo1"></div></div>
