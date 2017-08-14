803fc6fcca9d6b9f65a75912949bee9a00078b6f
Nuno Arruda <nuno@nunoarruda.com>
2017-08-14 14:37:50 +0200

    feat: add work page portfolio boxes

--- src/includes/pages/work.php
+++ src/includes/pages/work.php
@@ -10,3 +10,23 @@
 
     <p class="work-header-text text-center mx-auto">Our team can support your initiatives by developing design concepts, detailed interface designs and templates or by producing complete solutions from</p>
 </header>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="work-item col-12 col-md-8"><div class="work-item-container work-item-nye"></div></div>
+        <div class="work-item col-12 col-sm-6 col-md-4"><div class="work-item-container work-item-wagz"></div></div>
+        <div class="work-item col-12 col-sm-6 col-md-4"><div class="work-item-container work-item-amadeus"></div></div>
+        <div class="work-item col-12 col-md-8"><div class="work-item-container work-item-fpo1"></div></div>
+        <div class="work-item col-12 col-sm-6 col-md-4"><div class="work-item-container work-item-fpo2"></div></div>
+        <div class="work-item col-12 col-sm-6 col-md-4"><div class="work-item-container work-item-fpo3"></div></div>
+        <div class="work-item col-12 col-md-4"><div class="work-item-container work-item-fpo4"></div></div>
+        <div class="work-item col-12 col-md-8"><div class="work-item-container work-item-fpo5"></div></div>
+        <div class="work-item col-12 col-sm-6 col-md-4"><div class="work-item-container work-item-fpo6"></div></div>
+        <div class="work-item col-12 col-sm-6 col-md-4"><div class="work-item-container work-item-fpo7"></div></div>
+        <div class="work-item col-12 col-sm-6 col-md-4"><div class="work-item-container work-item-fpo8"></div></div>
+        <div class="work-item col-12 col-sm-6 col-md-4"><div class="work-item-container work-item-fpo9"></div></div>
+        <div class="work-item col-12 col-sm-6 col-md-4"><div class="work-item-container work-item-fpo10"></div></div>
+        <div class="work-item col-12 col-sm-6 col-md-4"><div class="work-item-container work-item-fpo11"></div></div>
+        <div class="work-item col-12 col-sm-6 col-md-4"><div class="work-item-container work-item-fpo11"></div></div>
+    </div>
+</div>
