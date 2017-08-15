1810d0daf2a62b9dad03ff9e26eab75afcf22b4a
Nuno Arruda <nuno@nunoarruda.com>
2017-08-15 13:41:53 +0200

    refactor: modularize home page

--- src/includes/pages/home.php
+++ src/includes/pages/home.php
@@ -1,108 +1,108 @@
-<div id="carouselHome" class="carousel slide" data-ride="carousel">
+<div id="home-carousel-header" class="carousel slide home-carousel-header" data-ride="carousel">
 
   <nav class="pl-nav">
     <a class="pl-nav-link" href="?page=about">About</a>
     <a class="pl-nav-link" href="#">Work</a>
   </nav>
 
-  <a href="/"><img id="pl-logo-hero" class="pl-logo-hero" src="images/logo-proximity-lab-white.svg" alt="Proximity Lab logo"></a>
+  <a href="/"><img class="home-carousel-header-logo" src="images/logo-proximity-lab-white.svg" alt="Proximity Lab logo"></a>
 
-  <div class="carousel-inner" role="listbox">
-    <div class="carousel-item carousel-item1 active">
-      <div class="carousel-caption d-none d-md-block">
-        <p class="carousel-text">Proximity Lab is an award-winning interaction design studio with a 25-year track record providing product strategy, user</p>
+  <div class="carousel-inner home-carousel-header-inner" role="listbox">
+    <div class="carousel-item home-carousel-header-item home-carousel-header-item1 active">
+      <div class="carousel-caption home-carousel-header-caption d-none d-md-block">
+        <p class="home-carousel-header-text">Proximity Lab is an award-winning interaction design studio with a 25-year track record providing product strategy, user</p>
       </div>
     </div>
-    <div class="carousel-item carousel-item1">
-      <div class="carousel-caption d-none d-md-block">
-        <p class="carousel-text">Proximity Lab is an award-winning interaction design studio with a 25-year track record providing product strategy, user</p>
+    <div class="carousel-item home-carousel-header-item home-carousel-header-item1">
+      <div class="carousel-caption home-carousel-header-caption d-none d-md-block">
+        <p class="home-carousel-header-text">Proximity Lab is an award-winning interaction design studio with a 25-year track record providing product strategy, user</p>
       </div>
     </div>
-    <div class="carousel-item carousel-item1">
-      <div class="carousel-caption d-none d-md-block">
-        <p class="carousel-text">Proximity Lab is an award-winning interaction design studio with a 25-year track record providing product strategy, user</p>
+    <div class="carousel-item home-carousel-header-item home-carousel-header-item1">
+      <div class="carousel-caption home-carousel-header-caption d-none d-md-block">
+        <p class="home-carousel-header-text">Proximity Lab is an award-winning interaction design studio with a 25-year track record providing product strategy, user</p>
       </div>
     </div>
   </div>
 
-  <img class="carousel-arrow" src="images/icon-arrow-bottom.svg" alt="Bottom arrow">
+  <img class="home-carousel-header-arrow" src="images/icon-arrow-bottom.svg" alt="Bottom arrow">
 </div>
 
 <div class="container-fluid">
   <div class="row">
     <div class="col-12">
-      <h3 class="pl-heading-md pl-color-tundora text-center mt-5 pt-4">Who we like to work with</h3>
+      <h1 class="home-workwith-header-heading text-center mt-5 pt-4">Who we like to work with</h1>
     </div>
 
-    <div class="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2">
-      <p class="pl-text-lg pl-color-dovegrey text-center workwith-text mt-3 mb-5 pb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor feugiat posuere. Vestibulum finibus neque vitae leo semper porttitor eu vel sapien. Donec sollicitudin augue sed nulla scelerisque, id tempor</p>
+    <div class="col-12 col-sm-10 mx-sm-auto col-md-8">
+      <p class="home-workwith-header-text text-center mt-3 mb-5 pb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor feugiat posuere. Vestibulum finibus neque vitae leo semper porttitor eu vel sapien. Donec sollicitudin augue sed nulla scelerisque, id tempor</p>
     </div>
   </div>
 
   <div class="row">
     <div class="col-12 col-sm-6 col-md-4">
-      <div class="workwith-item pb-5">
-        <img class="d-block mx-auto py-5 workwith-item-icon" src="images/icon-building.svg" alt="Building icon">
-        <h4 class="pl-heading-sm pl-color-fiord text-center workwith-item-heading">Enterprise UX</h4>
-        <p class="workwith-item-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor feugiat posuere. Vestibulum finibus neque vitae leo semper</p>
-        <img class="d-block mx-auto mt-5 workwith-item-arrow" src="images/icon-arrow-top-purple.svg" alt="Top arrow">
+      <div class="home-workwith-item pb-5">
+        <img class="home-workwith-item-icon d-block mx-auto py-5" src="images/icon-building.svg" alt="Building icon">
+        <h4 class="home-workwith-item-heading text-center">Enterprise UX</h4>
+        <p class="home-workwith-item-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor feugiat posuere. Vestibulum finibus neque vitae leo semper</p>
+        <img class="home-workwith-item-arrow d-block mx-auto mt-5" src="images/icon-arrow-top-purple.svg" alt="Top arrow">
       </div>
     </div>
     <div class="col-12 col-sm-6 col-md-4">
-      <div class="workwith-item pb-5">
-        <img class="d-block mx-auto py-5 workwith-item-icon" src="images/icon-desktop.svg" alt="Desktop icon">
-        <h4 class="pl-heading-sm pl-color-fiord text-center workwith-item-heading">Startups</h4>
-        <p class="workwith-item-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor feugiat posuere. Vestibulum finibus neque vitae leo semper</p>
-        <img class="d-block mx-auto mt-5 workwith-item-arrow" src="images/icon-arrow-top-purple.svg" alt="Top arrow">
+      <div class="home-workwith-item pb-5">
+        <img class="home-workwith-item-icon d-block mx-auto py-5" src="images/icon-desktop.svg" alt="Desktop icon">
+        <h4 class="home-workwith-item-heading text-center">Startups</h4>
+        <p class="home-workwith-item-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor feugiat posuere. Vestibulum finibus neque vitae leo semper</p>
+        <img class="home-workwith-item-arrow d-block mx-auto mt-5" src="images/icon-arrow-top-purple.svg" alt="Top arrow">
       </div>
     </div>
     <div class="col-12 col-sm-12 col-md-4">
-      <div class="workwith-item pb-5">
-        <img class="d-block mx-auto py-5 workwith-item-icon" src="images/icon-app.svg" alt="App icon">
-        <h4 class="pl-heading-sm pl-color-fiord text-center workwith-item-heading">Application Reboots</h4>
-        <p class="workwith-item-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor feugiat posuere. Vestibulum finibus neque vitae leo semper</p>
-        <img class="d-block mx-auto mt-5 workwith-item-arrow" src="images/icon-arrow-top-purple.svg" alt="Top arrow">
+      <div class="home-workwith-item pb-5">
+        <img class="home-workwith-item-icon d-block mx-auto py-5" src="images/icon-app.svg" alt="App icon">
+        <h4 class="home-workwith-item-heading text-center">Application Reboots</h4>
+        <p class="home-workwith-item-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor feugiat posuere. Vestibulum finibus neque vitae leo semper</p>
+        <img class="home-workwith-item-arrow d-block mx-auto mt-5" src="images/icon-arrow-top-purple.svg" alt="Top arrow">
       </div>
     </div>
   </div>
 </div>
 
-<div id="carouselTestimonials" class="carousel slide" data-ride="carousel">
+<div id="home-carousel-testimonial" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner" role="listbox">
-    <div class="carousel-item carousel-item-scribe active">
-      <div class="carousel-caption">
-        <h5 class="pl-heading-xs pl-color-mineshaft d-none d-md-block">THE BOND WITH OUR CLIENTS</h5>
-        <img class="testimonial-logo d-none d-lg-inline-block" src="images/logo-scribe.png" alt="Scribe logo">
-        <p class="testimonial-text">"I've been super impressed. I didn't have much of an idea what to expect going into this, with working with an outside web design firm as the closest prior experience. So I gotta say, I'm kind of blown away at how quickly things are progressing."</p>
-        <img class="testimonial-author d-none d-sm-inline-block" src="images/testimonial-author-scribe.png" alt="Testimonial author">
-        <p class="testimonial-author-title"><b>Shawn McGowen</b>, CEO Scribe</p>
+    <div class="carousel-item home-carousel-testimonial-item home-carousel-testimonial-item-scribe active">
+      <div class="carousel-caption home-carousel-testimonial-caption">
+        <h5 class="home-carousel-testimonial-heading d-none d-md-block">THE BOND WITH OUR CLIENTS</h5>
+        <img class="home-carousel-testimonial-logo-scribe d-none d-lg-inline-block" src="images/logo-scribe.png" alt="Scribe logo">
+        <p class="home-carousel-testimonial-text">"I've been super impressed. I didn't have much of an idea what to expect going into this, with working with an outside web design firm as the closest prior experience. So I gotta say, I'm kind of blown away at how quickly things are progressing."</p>
+        <img class="home-carousel-testimonial-author d-none d-sm-inline-block" src="images/testimonial-author-scribe.png" alt="Testimonial author">
+        <p class="home-carousel-testimonial-author-title"><b>Shawn McGowen</b>, CEO Scribe</p>
       </div>
     </div>
 
-    <div class="carousel-item carousel-item-adobe">
-      <div class="carousel-caption">
-        <h5 class="pl-heading-xs pl-color-mineshaft d-none d-md-block">THE BOND WITH OUR CLIENTS</h5>
-        <img class="testimonial-logo d-none d-lg-inline-block" src="images/logo-adobe.svg" alt="Adobe logo">
-        <p class="testimonial-text">"I've been super impressed. I didn't have much of an idea what to expect going into this, with working with an outside web design firm as the closest prior experience. So I gotta say, I'm kind of blown away at how quickly things are progressing."</p>
-        <img class="testimonial-author d-none d-sm-inline-block" src="images/testimonial-author-adobe.png" alt="Testimonial author">
-        <p class="testimonial-author-title"><b>Rachana Rele</b>, Director - User Experience</p>
+    <div class="carousel-item home-carousel-testimonial-item home-carousel-testimonial-item-adobe">
+      <div class="carousel-caption home-carousel-testimonial-caption">
+        <h5 class="home-carousel-testimonial-heading d-none d-md-block">THE BOND WITH OUR CLIENTS</h5>
+        <img class="home-carousel-testimonial-logo-adobe d-none d-lg-inline-block" src="images/logo-adobe.svg" alt="Adobe logo">
+        <p class="home-carousel-testimonial-text">"I've been super impressed. I didn't have much of an idea what to expect going into this, with working with an outside web design firm as the closest prior experience. So I gotta say, I'm kind of blown away at how quickly things are progressing."</p>
+        <img class="home-carousel-testimonial-author d-none d-sm-inline-block" src="images/testimonial-author-adobe.png" alt="Testimonial author">
+        <p class="home-carousel-testimonial-author-title"><b>Rachana Rele</b>, Director - User Experience</p>
       </div>
     </div>
 
-    <div class="carousel-item carousel-item-amadeus">
-      <div class="carousel-caption">
-        <h5 class="pl-heading-xs pl-color-mineshaft d-none d-md-block">THE BOND WITH OUR CLIENTS</h5>
-        <img class="testimonial-logo d-none d-lg-inline-block" src="images/logo-amadeus.svg" alt="Amadeus logo">
-        <p class="testimonial-text">"I've been super impressed. I didn't have much of an idea what to expect going into this, with working with an outside web design firm as the closest prior experience. So I gotta say, I'm kind of blown away at how quickly things are progressing."</p>
-        <img class="testimonial-author d-none d-sm-inline-block" src="images/testimonial-author-amadeus.png" alt="Testimonial author">
-        <p class="testimonial-author-title"><b>Lee Horgan</b>, CEO</p>
+    <div class="carousel-item home-carousel-testimonial-item home-carousel-testimonial-item-amadeus">
+      <div class="carousel-caption home-carousel-testimonial-caption">
+        <h5 class="home-carousel-testimonial-heading d-none d-md-block">THE BOND WITH OUR CLIENTS</h5>
+        <img class="home-carousel-testimonial-logo-amadeus d-none d-lg-inline-block" src="images/logo-amadeus.svg" alt="Amadeus logo">
+        <p class="home-carousel-testimonial-text">"I've been super impressed. I didn't have much of an idea what to expect going into this, with working with an outside web design firm as the closest prior experience. So I gotta say, I'm kind of blown away at how quickly things are progressing."</p>
+        <img class="home-carousel-testimonial-author d-none d-sm-inline-block" src="images/testimonial-author-amadeus.png" alt="Testimonial author">
+        <p class="home-carousel-testimonial-author-title"><b>Lee Horgan</b>, CEO</p>
       </div>
     </div>
   </div>
 
-  <ol class="carousel-indicators">
-    <li data-target="#carouselTestimonials" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselTestimonials" data-slide-to="1"></li>
-    <li data-target="#carouselTestimonials" data-slide-to="2"></li>
+  <ol class="carousel-indicators home-carousel-testimonial-indicators">
+    <li data-target="#home-carousel-testimonial" data-slide-to="0" class="home-carousel-testimonial-indicator active"></li>
+    <li data-target="#home-carousel-testimonial" data-slide-to="1" class="home-carousel-testimonial-indicator"></li>
+    <li data-target="#home-carousel-testimonial" data-slide-to="2" class="home-carousel-testimonial-indicator"></li>
   </ol>
 </div>
--- src/index.php
+++ src/index.php
@@ -11,8 +11,11 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Assistant:200,300,400,600">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" integrity="sha384-OHBBOqpYHNsIqQy8hL1U+8OXf9hH6QRxi0+EODezv82DfnZoV7qoHAZDwMwEJvSw" crossorigin="anonymous">
         <link rel="stylesheet" href="css/main.css">
-        <?php if ($activePage == 'about') echo '<link rel="stylesheet" href="css/about.css">'; ?>
-        <?php if ($activePage == 'work') echo '<link rel="stylesheet" href="css/work.css">'; ?>
+        <?php
+            if (!$activePage) echo '<link rel="stylesheet" href="css/home.css">';
+            if ($activePage == 'about') echo '<link rel="stylesheet" href="css/about.css">';
+            if ($activePage == 'work') echo '<link rel="stylesheet" href="css/work.css">';
+        ?>
     </head>
     <body class="animated fadeIn">
         <?php
