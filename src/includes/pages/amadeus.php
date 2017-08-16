a4bbfd2890c17adcc2193ed595b00d2eec2033fd
Nuno Arruda <nuno@nunoarruda.com>
2017-08-16 16:49:01 +0200

    refactor: modularize amadeus page portfolio section

--- src/includes/pages/amadeus.php
+++ src/includes/pages/amadeus.php
@@ -23,9 +23,9 @@
 <div class="container-fluid my-5 pt-5">
     <div class="row">
         <div class="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3 text-center">
-            <h5 class="pl-heading-xs pl-color-boulder service-info-heading1">PORTFOLIO</h5>
-            <h3 class="pl-heading-md pl-color-mineshaft service-info-heading2">Planning spaces for the future</h3>
-            <p class="pl-text-lg pl-color-boulder service-info-paragraph">Our team can support your initiatives by developing design concepts, detailed interface designs and templates or by producing complete solutions from</p>
+            <h5 class="ama-portfolio-heading-secondary">PORTFOLIO</h5>
+            <h3 class="ama-portfolio-heading-primary">Planning spaces for the future</h3>
+            <p class="ama-portfolio-paragraph">Our team can support your initiatives by developing design concepts, detailed interface designs and templates or by producing complete solutions from</p>
         </div>
     </div>
 </div>
@@ -33,24 +33,24 @@
 <div class="container-fluid mb-5 pb-5">
     <div class="row">
         <div class="col-12 col-sm-4 align-self-center text-center">
-            <div class="how-info">
-                <img class="how-circle" src="images/icon-think.svg" alt="Think icon">
-                <h4 class="pl-heading-sm pl-color-tundora how-heading">Designing</h4>
-                <p class="pl-text-md pl-color-boulder how-text">Our team can support your initiatives by developing design concepts, detailed interface designs and templates or by</p>
+            <div class="ama-portfolio-service">
+                <img class="ama-portfolio-service-icon" src="images/icon-think.svg" alt="Think icon">
+                <h4 class="ama-portfolio-service-heading">Designing</h4>
+                <p class="ama-portfolio-service-text">Our team can support your initiatives by developing design concepts, detailed interface designs and templates or by</p>
             </div>
         </div>
         <div class="col-12 col-sm-4 align-self-center text-center">
-            <div class="how-info">
-                <img class="how-circle" src="images/icon-plan.svg" alt="Plan icon">
-                <h4 class="pl-heading-sm pl-color-tundora how-heading">Planning</h4>
-                <p class="pl-text-md pl-color-boulder how-text">Our team can support your initiatives by developing design concepts, detailed interface designs and templates or by</p>
+            <div class="ama-portfolio-service">
+                <img class="ama-portfolio-service-icon" src="images/icon-plan.svg" alt="Plan icon">
+                <h4 class="ama-portfolio-service-heading">Planning</h4>
+                <p class="ama-portfolio-service-text">Our team can support your initiatives by developing design concepts, detailed interface designs and templates or by</p>
             </div>
         </div>
         <div class="col-12 col-sm-4 align-self-center text-center">
-            <div class="how-info">
-                <img class="how-circle" src="images/icon-bulb.svg" alt="Bulb icon">
-                <h4 class="pl-heading-sm pl-color-tundora how-heading">Problem Solving</h4>
-                <p class="pl-text-md pl-color-boulder how-text">Our team can support your initiatives by developing design concepts, detailed interface designs and templates or by</p>
+            <div class="ama-portfolio-service">
+                <img class="ama-portfolio-service-icon" src="images/icon-bulb.svg" alt="Bulb icon">
+                <h4 class="ama-portfolio-service-heading">Problem Solving</h4>
+                <p class="ama-portfolio-service-text">Our team can support your initiatives by developing design concepts, detailed interface designs and templates or by</p>
             </div>
         </div>
     </div>
