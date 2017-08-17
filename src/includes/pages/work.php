22c36f3a501a82caf7bbec5b4df268887076fe94
Nuno Arruda <nuno@nunoarruda.com>
2017-08-17 17:11:59 +0200

    feat: add work page project hover state

--- src/includes/pages/work.php
+++ src/includes/pages/work.php
@@ -12,23 +12,35 @@
     <div class="row">
         <div class="work-item col-12 col-md-8">
             <div class="work-item-container work-item-nye text-center">
-                <img class="work-item-logo work-item-logo-nye" src="images/logo-nye.png" alt="Nye logo">
-                <p class="work-item-text mx-auto">Our team can support your initiatives by developing design concepts, detailed interface designs and templates or by producing complete</p>
-                <a class="work-item-button" href="?page=nye">View work</a>
+                <div class="work-item-overlay work-item-overlay-nye">
+                    <div class="work-item-wrapper">
+                        <img class="work-item-logo work-item-logo-nye" src="images/logo-nye.png" alt="Nye logo">
+                        <p class="work-item-text mx-auto d-none d-sm-block">Our team can support your initiatives by developing design concepts, detailed interface designs and templates or by producing complete</p>
+                        <a class="work-item-button" href="?page=nye">View work</a>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="work-item col-12 col-sm-6 col-md-4">
             <div class="work-item-container work-item-wagz text-center">
-                <img class="work-item-logo work-item-logo-wagz" src="images/logo-wagz-green.png" alt="Wagz logo">
-                <p class="work-item-text mx-auto">Our team can support your initiatives by developing design concepts, detailed interface designs and templates or by producing complete</p>
-                <a class="work-item-button" href="?page=wagz">View work</a>
+                <div class="work-item-overlay work-item-overlay-wagz">
+                    <div class="work-item-wrapper">
+                        <img class="work-item-logo work-item-logo-wagz" src="images/logo-wagz-green.png" alt="Wagz logo">
+                        <p class="work-item-text mx-auto d-none d-sm-block">Our team can support your initiatives by developing design concepts, detailed interface designs and templates or by producing complete</p>
+                        <a class="work-item-button" href="?page=wagz">View work</a>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="work-item col-12 col-sm-6 col-md-4">
             <div class="work-item-container work-item-amadeus text-center">
-                <img class="work-item-logo work-item-logo-amadeus" src="images/logo-amadeus.svg" alt="Amadeus logo">
-                <p class="work-item-text mx-auto">Our team can support your initiatives by developing design concepts, detailed interface designs and templates or by producing complete</p>
-                <a class="work-item-button" href="?page=amadeus">View work</a>
+                <div class="work-item-overlay work-item-overlay-amadeus">
+                    <div class="work-item-wrapper">
+                        <img class="work-item-logo work-item-logo-amadeus" src="images/logo-amadeus.svg" alt="Amadeus logo">
+                        <p class="work-item-text d-none d-sm-block">Our team can support your initiatives by developing design concepts, detailed interface designs and templates or by producing complete</p>
+                        <a class="work-item-button" href="?page=amadeus">View work</a>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="work-item col-12 col-md-8"><div class="work-item-container work-item-fpo1"></div></div>
