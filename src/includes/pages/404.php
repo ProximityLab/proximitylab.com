8db5d8d5dd49b912c03617699c6d75f9207288eb
Josh Rose <joshdavis@Joshs-MacBook-Pro.local>
2017-09-10 18:41:28 -0400

    Consolidated index logic. Added 404 page and CSS.

---
+++ src/includes/pages/404.php
@@ -0,0 +1,7 @@
+<header class="page404-header">
+    <div class="page404-header-message">
+        <h1 class="page404-header-heading text-center">Zoiks! Error 404</h1>
+
+        <p class="page404-header-text text-center mx-auto">Sorry, this page doesn't exist.</p>
+    </div>
+</header>
--- src/index.php
+++ src/index.php
@@ -1,19 +1,98 @@
 <?php
     $activePage = !empty($_GET['page']) ? trim($_GET['page']) : '';
 
-    if ($activePage === '/work') {
-      $bodyclass = 'ww-work';
-    }
-    else if($activePage === '' || $activePage === '/home')
-    {
-      $bodyclass = 'home';
-    }
-    else if($activePage === '/about')
-    {
-      $bodyclass = 'about';
-    }
-    else {
-      $bodyclass = 'ww-portfolio';
+    $bodyclass = 'ww-portfolio'; // default body class
+    $activePagePhp = 'includes/pages/home.php'; // default active page include
+    $pageCss = '<link rel="stylesheet" href="/css/home.css">'; //default page css
+
+    switch ($activePage) {
+      case '/work/solidworks':
+        $pageCss = '<link rel="stylesheet" href="/css/solidworks.css">';
+        $activePagePhp = 'includes/pages/solidworks.php';
+        break;
+      case '/work/mosaic':
+        $pageCss = '<link rel="stylesheet" href="/css/mosaic.css">';
+        $activePagePhp = 'includes/pages/mosaic.php';
+        break;
+      case '/work/rootinsight':
+        $pageCss = '<link rel="stylesheet" href="/css/rootinsight.css">';
+        $activePagePhp = 'includes/pages/rootinsight.php';
+        break;
+      case '/work/adobe':
+        $pageCss = '<link rel="stylesheet" href="/css/adobe.css">';
+        $activePagePhp = 'includes/pages/adobe.php';
+        break;
+      case '/work/eagle':
+        $pageCss = '<link rel="stylesheet" href="/css/eagle.css">';
+        $activePagePhp = 'includes/pages/eagle.php';
+        break;
+      case '/work/philips':
+        $pageCss = '<link rel="stylesheet" href="/css/philips.css">';
+        $activePagePhp = 'includes/pages/philips.php';
+        break;
+      case '/work/scribe':
+        $pageCss = '<link rel="stylesheet" href="/css/scribe.css">';
+        $activePagePhp = 'includes/pages/scribe.php';
+        break;
+      case '/work/minim':
+        $pageCss = '<link rel="stylesheet" href="/css/minim.css">';
+        $activePagePhp = 'includes/pages/minim.php';
+        break;
+      case '/work/icovia':
+        $pageCss = '<link rel="stylesheet" href="/css/icovia.css">';
+        $activePagePhp = 'includes/pages/icovia.php';
+        break;
+      case '/work/scheduleme':
+        $pageCss = '<link rel="stylesheet" href="/css/scheduleme.css">';
+        $activePagePhp = 'includes/pages/scheduleme.php';
+        break;
+      case '/work/songbird':
+        $pageCss = '<link rel="stylesheet" href="/css/songbird.css">';
+        $activePagePhp = 'includes/pages/songbird.php';
+        break;
+      case '/work/billnye':
+        $pageCss = '<link rel="stylesheet" href="/css/nye.css">';
+        $activePagePhp = 'includes/pages/billnye.php';
+        break;
+      case '/work/wagz':
+        $pageCss = '<link rel="stylesheet" href="/css/wagz.css">';
+        $activePagePhp = 'includes/pages/wagz.php';
+        break;
+      case '/work/amadeus':
+        $pageCss = '<link rel="stylesheet" href="/css/amadeus.css">';
+        $activePagePhp = 'includes/pages/amadeus.php';
+        break;
+      case '':
+      case '/home':
+        $bodyclass = 'home';
+        $pageCss = '<link rel="stylesheet" href="/css/home.css">';
+        $activePagePhp = 'includes/pages/home.php';
+        break;
+      case '/about':
+        $bodyclass = 'about';
+        $pageCss = '<link rel="stylesheet" href="/css/about.css">';
+        $activePagePhp = 'includes/pages/about.php';
+        break;
+      case '/work':
+        $bodyclass = 'ww-work';
+        $pageCss = '<link rel="stylesheet" href="/css/work.css">';
+        $activePagePhp = 'includes/pages/work.php';
+        break;
+      case '/404':
+        $bodyclass = 'ww-work';
+        $pageCss = '<link rel="stylesheet" href="/css/404.css">';
+        $activePagePhp = 'includes/pages/404.php';
+        break;
+      default:
+        if (empty($activePage)) { // If activePage is empty then we assume it's home
+          $bodyclass = 'home';
+          $pageCss = '<link rel="stylesheet" href="/css/home.css">';
+          $activePagePhp = 'includes/pages/home.php';
+        } else { // If activePage isn't empty, but doesn't match anything else then it's 404
+          header('Location: /?page=/404');
+          exit;
+        }
+        break;
     }
 ?>
 <!doctype html>
@@ -22,29 +101,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>Proximity Lab │ Human-centered Experience Design, User Interface and Product Launch</title>
+
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Assistant:200,300,400,600">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" integrity="sha384-OHBBOqpYHNsIqQy8hL1U+8OXf9hH6QRxi0+EODezv82DfnZoV7qoHAZDwMwEJvSw" crossorigin="anonymous">
         <link rel="stylesheet" href="/css/main.css">
-        <?php
-            if (empty($activePage)) echo '<link rel="stylesheet" href="/css/home.css">';
-            if ($activePage == '/work/solidworks') echo '<link rel="stylesheet" href="/css/solidworks.css">';
-            if ($activePage == '/work/mosaic') echo '<link rel="stylesheet" href="/css/mosaic.css">';
-            if ($activePage == '/work/rootinsight') echo '<link rel="stylesheet" href="/css/rootinsight.css">';
-            if ($activePage == '/work/adobe') echo '<link rel="stylesheet" href="/css/adobe.css">';
-            if ($activePage == '/work/eagle') echo '<link rel="stylesheet" href="/css/eagle.css">';
-            if ($activePage == '/work/philips') echo '<link rel="stylesheet" href="/css/philips.css">';
-            if ($activePage == '/work/scribe') echo '<link rel="stylesheet" href="/css/scribe.css">';
-            if ($activePage == '/work/minim') echo '<link rel="stylesheet" href="/css/minim.css">';
-            if ($activePage == '/work/icovia') echo '<link rel="stylesheet" href="/css/icovia.css">';
-            if ($activePage == '/work/scheduleme') echo '<link rel="stylesheet" href="/css/scheduleme.css">';
-            if ($activePage == '/work/songbird') echo '<link rel="stylesheet" href="/css/songbird.css">';
-            if ($activePage == '/about') echo '<link rel="stylesheet" href="/css/about.css">';
-            if ($activePage == '/work') echo '<link rel="stylesheet" href="/css/work.css">';
-            if ($activePage == '/work/billnye') echo '<link rel="stylesheet" href="/css/nye.css">';
-            if ($activePage == '/work/wagz') echo '<link rel="stylesheet" href="/css/wagz.css">';
-            if ($activePage == '/work/amadeus') echo '<link rel="stylesheet" href="/css/amadeus.css">';
-        ?>
+        <?= $pageCss ?>
+
         <script>
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -57,78 +120,26 @@
         </script>
     </head>
     <body id="waypoint" class="animated fadeIn <?php echo $bodyclass ?>">
-      <?php include('includes/navigation.php'); ?>
-        <?php
-            switch ($activePage) {
-                case '/work/solidworks':
-                  include('includes/pages/solidworks.php');
-                  break;
-                case '/work/mosaic':
-                  include('includes/pages/mosaic.php');
-                  break;
-                case '/work/rootinsight':
-                  include('includes/pages/rootinsight.php');
-                  break;
-                case '/work/adobe':
-                  include('includes/pages/adobe.php');
-                  break;
-                case '/work/eagle':
-                  include('includes/pages/eagle.php');
-                  break;
-                case '/work/philips':
-                  include('includes/pages/philips.php');
-                  break;
-                case '/work/minim':
-                  include('includes/pages/minim.php');
-                  break;
-                case '/work/icovia':
-                  include('includes/pages/icovia.php');
-                  break;
-                case '/work/scribe':
-                  include('includes/pages/scribe.php');
-                  break;
-                case '/work/scheduleme':
-                  include('includes/pages/scheduleme.php');
-                  break;
-                case '/work/songbird':
-                  include('includes/pages/songbird.php');
-                  break;
-                case '/work/amadeus':
-                  include('includes/pages/amadeus.php');
-                  break;
-                case '/work/wagz':
-                  include('includes/pages/wagz.php');
-                  break;
-                case '/work/billnye':
-                  include('includes/pages/billnye.php');
-                  break;
-                case '/work':
-                  include('includes/pages/work.php');
-                  break;
-                case '/about':
-                  include('includes/pages/about.php');
-                  break;
-                default:
-                  include('includes/pages/home.php');
-            }
-        ?>
+      <?php
+        include('includes/navigation.php');
+        include($activePagePhp);
+        include('includes/footer.php');
+      ?>
 
-        <?php include('includes/footer.php'); ?>
-
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-        <script src="/js/jquery.waypoints.min.js"></script>
-        <!-- <script>
-          if ('registerElement' in document
-            && 'createShadowRoot' in HTMLElement.prototype
-            && 'import' in document.createElement('link')
-            && 'content' in document.createElement('template')) {
-            // We're using a browser with native WC support!
-          } else {
-            document.write('<script src="https:\/\/cdnjs.cloudflare.com/ajax/libs/polymer/0.3.4/platform.js"><\/script>')
-          }
-        </script> -->
-        <script src="/custom.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+      <script src="/js/jquery.waypoints.min.js"></script>
+      <!-- <script>
+        if ('registerElement' in document
+          && 'createShadowRoot' in HTMLElement.prototype
+          && 'import' in document.createElement('link')
+          && 'content' in document.createElement('template')) {
+          // We're using a browser with native WC support!
+        } else {
+          document.write('<script src="https:\/\/cdnjs.cloudflare.com/ajax/libs/polymer/0.3.4/platform.js"><\/script>')
+        }
+      </script> -->
+      <script src="/custom.js"></script>
     </body>
 </html>
