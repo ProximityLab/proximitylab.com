16fb9fe7fe7270d12f4571701f63f3ba256b2e4a
Josh Rose <joshdavis@Joshs-MacBook-Pro.local>
2017-09-21 21:56:03 -0400

    Fixed minim video layout.

--- src/includes/pages/minim.php
+++ src/includes/pages/minim.php
@@ -6,7 +6,7 @@
               <img class="jumbotron-logo-project" src="/images/logo-minim-white-169x60.png" alt="Minim logo">
               <a href="#start" class="scroll-down-arrow">Read more about Minim</a>
             </div>
-            <div class="col-lg-5 push-xl-2 push-lg-2 ww-hero-image">
+            <div class="col-12 col-lg-5 push-xl-2 push-lg-2 ww-hero-image">
             <div class="ww-vid-holder"><div class="ww-vid">
                   <video id="video1" poster="/images/video-poster-minim-2x.png" autoplay>
                   </video>
@@ -23,7 +23,7 @@
 
       <h1 class="heading-primary text-center">Demystifying the IoT</h1>
 
-      <p class="header-text text-center">A much-needed app for simplifying & securing smart homes</p>
+      <p class="header-text text-center">A much-needed app for simplifying &amp; securing smart homes</p>
     </div>
   </div>
 </div>
@@ -31,7 +31,13 @@
 <div class="container-fluid project-description">
   <div class="row">
     <div class="col-12">
-      <p>With the dramatic increase of IoT devices on home networks comes an even greater need to manage and protect them. The problem is two-fold. First, each device has unique settings and has the potential to introduce vulnerabilities that can affect not only the home network. Second, if devices are unsecured on home networks, they have the potential to interrupt network operations for ISPs to the point where entire portions of the Internet are down. As Minim looked to create a solution that would help protect consumers and ISPs, they turned to our team to help design the required interfaces.</p><p>As partners with the Minim team, we worked together to fully understand the challenges we had to solve in order to design the appropriate product experiences.</p><h3>ISPs have limited visibility into home networks.</h3><p> While their job is to provide a reliable service to customers at all times, there are many factors out of their control. Most ISPs can only tell if a the cable modem is on-line or off-line - limiting their ability to assist customers in troubleshooting problems or diagnosing broader network or device problems. They required a view that would aggregate information from different systems about the customer and their networked devices.</p><h3>Consumers are not technical experts.</h3><p>Sometimes smart homes aren’t really that smart. Although many consumers are quite adept at working with their personal devices, the sheer number and type of devices on home networks can be baffling. Each manufacturer has their own set-up and configuration instructions for their devices. Making matters more complex is that each device needs to be updated periodically to help keep it operational and secure. These steps are overwhelming for average home network users.</p><p>Our approach to designing an experience with Minim included multiple steps, including:</p>
+      <p>With the dramatic increase of IoT devices on home networks comes an even greater need to manage and protect them. The problem is two-fold. First, each device has unique settings and has the potential to introduce vulnerabilities that can affect not only the home network. Second, if devices are unsecured on home networks, they have the potential to interrupt network operations for ISPs to the point where entire portions of the Internet are down. As Minim looked to create a solution that would help protect consumers and ISPs, they turned to our team to help design the required interfaces.</p>
+      <p>As partners with the Minim team, we worked together to fully understand the challenges we had to solve in order to design the appropriate product experiences.</p>
+      <h3>ISPs have limited visibility into home networks.</h3>
+      <p> While their job is to provide a reliable service to customers at all times, there are many factors out of their control. Most ISPs can only tell if a the cable modem is on-line or off-line - limiting their ability to assist customers in troubleshooting problems or diagnosing broader network or device problems. They required a view that would aggregate information from different systems about the customer and their networked devices.</p>
+      <h3>Consumers are not technical experts.</h3>
+      <p>Sometimes smart homes aren’t really that smart. Although many consumers are quite adept at working with their personal devices, the sheer number and type of devices on home networks can be baffling. Each manufacturer has their own set-up and configuration instructions for their devices. Making matters more complex is that each device needs to be updated periodically to help keep it operational and secure. These steps are overwhelming for average home network users.</p>
+      <p>Our approach to designing an experience with Minim included multiple steps, including:</p>
     </div>
 
   </div>
@@ -43,7 +49,7 @@
     <div id="map" class="row">
       <div class="col-lg-6 half-image flex-first"></div>
       <div class="col-lg-6 d-flex flex-column flex-first text-container">
-        <h2>Persona & journey map development</h2>
+        <h2>Persona &amp; journey map development</h2>
         <p>Understanding who would be using Minim was the most important step in designing the appropriate experiences. On the consumer side, we identified a user with only the most basic technical knowledge, but who would be able to plug in and configure the Minim solution. ISPs have dedicated support teams, but most don’t have tools designed that give a holistic view of the network with the ability to drill down into individual home networks to identify problematic devices and configurations. For both of these personas we created journey maps and typical use cases for how they would interact with the Minim solution.</p>
       </div>
     </div>
@@ -70,7 +76,7 @@
     <div class="ww-content">
         <p>"Proximity Lab is at the top of the game for user experience and design. Having worked with them on multiple occasions and projects, they have proven to be a firm that goes way beyond producing simple mock-ups. They take the time to understand the core solution a product is to deliver."</p><img src="/images/profile-jeremy-hitchcock-2x.png" alt="Testimonial author">
       <p class="home-carousel-testimonial-author-title">
-          <strong>Jeremy Hitchcock,</strong> Founder & CEO  (Minim & Dyn)</p>
+          <strong>Jeremy Hitchcock,</strong> Founder &amp; CEO  (Minim &amp; Dyn)</p>
     </div>
   </div>
 </div>
