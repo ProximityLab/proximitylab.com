62dda5536add9606d4498c0e27968d332e370426
Nuno Arruda <nuno@nunoarruda.com>
2018-06-15 15:31:41 +0000

    feat: add testimonials

--- src/includes/pages/home-new.php
+++ src/includes/pages/home-new.php
@@ -194,3 +194,52 @@
         </div>
     </div>
 </div>
+
+
+<!-- Testimonials -->
+<div id="home-carousel-testimonial" class="carousel carousel-fade" data-ride="carousel" data-interval="10000">
+    <div class="carousel-inner">
+        <div class="carousel-item home-carousel-testimonial-item home-carousel-testimonial-item-adobe active">
+            <div class="carousel-caption home-carousel-testimonial-caption">
+                <img class="home-carousel-testimonial-logo-adobe" src="/images/logo-adobe.svg" alt="Adobe logo">
+                <p class="home-carousel-testimonial-text">“As Adobe has grown and expanded its product lines over the years, we have relied on the outside expertise of Proximity Lab to help ensure we are designing and developing the most usable and effective tools possible for the creative marketplace. In many cases, they are not only a trusted partner, but serve as an extension to our design team to help on everything from product-focused UI exercises to executive level vision work.”</p>
+                <img class="home-carousel-testimonial-author" src="/images/profile-jamie-myrold-2x.png" alt="Testimonial author">
+                <p class="home-carousel-testimonial-author-title"><b>Jamie Myrold</b><br>VP of Design Adobe</p>
+            </div>
+        </div>
+
+        <div class="carousel-item home-carousel-testimonial-item home-carousel-testimonial-item-scribe">
+            <div class="carousel-caption home-carousel-testimonial-caption">
+                <img class="home-carousel-testimonial-logo-scribe" src="/images/logo-scribe.png" alt="Scribe logo">
+                <p class="home-carousel-testimonial-text">“Proximity Lab understands our business - they challenged our thinking to help create new paradigms for data integration. The resulting user-centric design is now driving the product roadmap for our SaaS platform - we are seeing a new category of users performing data integration tasks that would have previously been outside of their skillset.”</p>
+                <img class="home-carousel-testimonial-author" src="/images/testimonial-author-scribe-2x.png" alt="Testimonial author">
+                <p class="home-carousel-testimonial-author-title"><b>Shawn McGowan</b><br>CEO Scribe</p>
+            </div>
+        </div>
+
+        <div class="carousel-item home-carousel-testimonial-item home-carousel-testimonial-item-eagle">
+            <div class="carousel-caption home-carousel-testimonial-caption">
+                <img class="home-carousel-testimonial-logo-eagle" src="/images/logo-eagle.svg" alt="Eagle logo">
+                <p class="home-carousel-testimonial-text">“Proximity Lab was an excellent UX/UI product strategy and design partner. They helped us accelerate our understanding of user journeys and user profiles, and ultimately created stunning visual designs that enabled us to demonstrate the power of our product to current and new customers.”</p>
+                <img class="home-carousel-testimonial-author" src="/images/profile_ureta_vidal_2x.png" alt="Testimonial author">
+                <p class="home-carousel-testimonial-author-title"><b>Abel Ureta-Vidal</b><br>CEO Eagle Genomics</p>
+            </div>
+        </div>
+
+        <div class="carousel-item home-carousel-testimonial-item home-carousel-testimonial-item-minim">
+            <div class="carousel-caption home-carousel-testimonial-caption">
+                <img class="home-carousel-testimonial-logo-minim" src="/images/logo-minim-white-169x60.png" alt="Minim logo">
+                <p class="home-carousel-testimonial-text">“Proximity Lab is at the top of the game for user experience and design. Having worked with them on multiple occasions and projects, they have proven to be a firm that goes way beyond producing simple mock-ups. They take the time to understand the core solution a product needs to deliver.”</p>
+                <img class="home-carousel-testimonial-author" src="/images/profile-jeremy-hitchcock-2x.png" alt="Testimonial author">
+                <p class="home-carousel-testimonial-author-title"><b>Jeremy Hitchcock</b><br>Founder &amp; COO Minim</p>
+            </div>
+        </div>
+    </div>
+
+    <ol class="carousel-indicators home-carousel-testimonial-indicators">
+        <li data-target="#home-carousel-testimonial" data-slide-to="0" class="home-carousel-testimonial-indicator active"></li>
+        <li data-target="#home-carousel-testimonial" data-slide-to="1" class="home-carousel-testimonial-indicator"></li>
+        <li data-target="#home-carousel-testimonial" data-slide-to="2" class="home-carousel-testimonial-indicator"></li>
+        <li data-target="#home-carousel-testimonial" data-slide-to="3" class="home-carousel-testimonial-indicator"></li>
+    </ol>
+</div>
