322e2325982ef71276c2a7d42a413c5c81d1875f
Nuno Arruda <nuno@nunoarruda.com>
2018-07-16 14:42:44 +0000

    refactor: style changes on global footer

--- src/includes/footer-new.php
+++ src/includes/footer-new.php
@@ -1,24 +1,22 @@
 <!-- Global footer -->
-<footer class="container text-center py-5 my-5">
-    <div class="row py-5 my-5">
+<footer class="container text-center pt-5 pb-4 my-5">
+    <div class="row pt-3">
         <div class="col-12">
+            <h4 class="global-footer-heading py-3">Re-think your experience.</h4>
+        </div>
+
+        <div class="col-12 pt-4 pb-5">
             <a href="#" class="global-footer-link">+ Get in Touch</a>
         </div>
 
         <div class="col-12">
-            <h4 class="global-footer-heading my-5 py-4">Re-think your experience.</h4>
-        </div>
-
-        <div class="col-12">
             <p class="global-footer-info">
-                © 2008-2018 Proximity Lab
+                <a href="tel:8557769522" class="global-footer-phone">(855) 776 - 9522</a>
                 <span class="global-footer-separator">|</span>
-                <a href="tel:8557769522" class="global-footer-phone">855.776.9522</a>
-                <span class="global-footer-separator">|</span>
-                <a class="global-footer-email" href="mailto:info@proximitylab.com">info@proximitylab.com</a>
-                <span class="global-footer-separator">|</span>
-                <a target="_blank" href="https://twitter.com/proximitylab" class="mr-3"><img class="global-footer-social-logo" src="/images/logo-twitter.svg"></a>
-                <a target="_blank" href="https://www.behance.net/proximitylab"><img class="global-footer-social-logo" src="/images/logo-be.svg"></a>
+                <a class="global-footer-email" href="mailto:info@proximitylab.com">info@proximitylab.com</a><br>
+                <a target="_blank" href="https://twitter.com/proximitylab" class="d-inline-block mt-3 mr-3"><img class="global-footer-social-logo" src="/images/logo-twitter.svg"></a>
+                <a target="_blank" href="https://www.behance.net/proximitylab"><img class="global-footer-social-logo" src="/images/logo-be.svg"></a><br>
+                <span class="d-block pt-5 mt-4">© 2008-2018 Proximity Lab</span>
             </p>
         </div>
     </div>
