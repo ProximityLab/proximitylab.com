bac8809264479171e7e87c4c58803af463f885c6
Nuno Arruda <nuno@nunoarruda.com>
2018-07-16 17:49:59 +0000

    feat: add load/scroll animations from current website

--- src/includes/pages/home-new.php
+++ src/includes/pages/home-new.php
@@ -1,5 +1,5 @@
 <!-- Header -->
-<div class="container mt-5 mt-sm-5 pt-sm-4">
+<div class="container mt-5 mt-sm-5 pt-sm-4 module-fade">
     <div class="row">
         <div class="col-3 col-md-5">
             <img src="/images/logo-proximity-lab-2018.svg" class="home-proximity-lab-logo d-md-none ml-4" alt="Proximity Lab logo">
@@ -16,7 +16,7 @@
 
 
 <!-- Main heading -->
-<div class="container">
+<div class="container module-fade">
     <div class="row">
         <div class="col">
             <h1 class="home-main-heading text-center text-md-left">Listen. Create. Reboot.</h1>
@@ -26,7 +26,7 @@
 
 
 <!-- Services carousel xs and sm -->
-<div class="carousel slide mt-4 d-md-none" data-ride="carousel" data-interval="3000">
+<div class="carousel slide mt-4 d-md-none module-fade" data-ride="carousel" data-interval="3000">
   <div class="carousel-inner text-center">
     <div class="carousel-item active">
         <img src="/images/icon-service-research.svg" class="home-icon-service">
@@ -45,21 +45,21 @@
 
 
 <!-- Services md and up -->
-<div class="container mt-2 d-none d-md-block">
-    <div class="row text-center text-md-left">
-        <div class="col-12 col-md-4 py-4 pt-md-0">
+<div class="container mt-2 d-none d-md-block module-fade">
+    <div class="row text-center text-md-left module">
+        <div class="col-12 col-md-4 py-4 pt-md-0 module">
             <img src="/images/icon-service-research.svg" class="home-icon-service">
             <h2 class="home-service-heading px-5 px-md-0">Research for Deep Understanding</h2>
             <a class="home-service-link" href="#">+ Learn More</a>
         </div>
 
-        <div class="col-12 col-md-4 py-4 pt-md-0">
+        <div class="col-12 col-md-4 py-4 pt-md-0 module">
             <img src="/images/icon-service-strategy.svg" class="home-icon-service">
             <h2 class="home-service-heading px-5 px-md-0 mr-xl-5">Business-centric Product Strategy</h2>
             <a class="home-service-link" href="#">+ Learn More</a>
         </div>
 
-        <div class="col-12 col-md-4 py-4 pt-md-0">
+        <div class="col-12 col-md-4 py-4 pt-md-0 module">
             <img src="/images/icon-service-design.svg" class="home-icon-service">
             <h2 class="home-service-heading px-5 px-md-0 mr-xl-5">Results-based UX and UI Design</h2>
             <a class="home-service-link" href="#">+ Learn More</a>
@@ -69,10 +69,10 @@
 
 
 <!-- Minim -->
-<div class="container mb-4 pb-4 mb-lg-0 pb-lg-0">
+<div class="container mb-4 pb-4 mb-lg-0 pb-lg-0 module-fade">
     <div class="row">
         <div class="col-12 col-md col-lg-5 offset-lg-1 col-xl-6 pr-xl-0">
-            <img class="home-minim-mobile-app-mockup d-block m-auto" src="/images/minim-mobile-app-mockup.png" alt="Minim mobile app mockup">
+            <img class="home-minim-mobile-app-mockup d-block m-auto module-fade" src="/images/minim-mobile-app-mockup.png" alt="Minim mobile app mockup">
 
             <div class="home-minim-video-wrapper m-auto">
                 <video id="minim-video" class="home-minim-video" poster="/images/home-minim-video-poster.jpg">
@@ -89,9 +89,9 @@
             </div>
         </div>
         <div class="col-12 col-md col-lg-5 col-xl-4 pl-sm-0 align-self-center home-minim-copy">
-            <h1 class="home-minim-heading-primary mx-4 px-2 mx-md-0 px-md-0 mb-4 mb-md-5">Demystifying the Internet of Things</h1>
-            <h2 class="home-minim-heading-secondary mx-4 px-2 mx-md-0 px-md-0 mb-2 mb-md-4">Empowering consumers with smart home security</h2>
-            <a class="home-minim-link mx-4 px-2 mx-md-0 px-md-0" href="#">+ Read the Case Study</a>
+            <h1 class="home-minim-heading-primary mx-4 px-2 mx-md-0 px-md-0 mb-4 mb-md-5 module">Demystifying the Internet of Things</h1>
+            <h2 class="home-minim-heading-secondary mx-4 px-2 mx-md-0 px-md-0 mb-2 mb-md-4 module">Empowering consumers with smart home security</h2>
+            <a class="home-minim-link mx-4 px-2 mx-md-0 px-md-0 module" href="#">+ Read the Case Study</a>
         </div>
     </div>
 </div>
@@ -100,7 +100,7 @@
 <!-- Oracle -->
 <div class="container-fluid my-4 py-4 my-lg-0 py-lg-0">
     <div class="row home-oracle-row">
-        <div class="col-12 col-lg-auto">
+        <div class="col-12 col-lg-auto module">
             <img class="home-oracle-image d-block" src="/images/home-oracle-image.png" alt="Oracle image">
 
             <div class="home-oracle-video-wrapper m-auto">
@@ -118,9 +118,9 @@
             </div>
         </div>
         <div class="col-12 col-lg-5 col-xl-3 pl-xl-0 align-self-center">
-            <h1 class="home-oracle-heading-primary mx-4 px-2 mx-md-0 px-md-0 mb-4 mb-md-5">Platform Storytelling</h1>
-            <h2 class="home-oracle-heading-secondary mx-4 px-2 mx-md-0 px-md-0 mb-2 mb-md-4">Helping a software giant persuade with compelling stories of transformation</h2>
-            <a class="home-oracle-link mx-3 px-2 mx-md-0 px-md-0" href="#" id="oracle-video-play-link"><i class="material-icons">play_arrow</i> Play the Video</a>
+            <h1 class="home-oracle-heading-primary mx-4 px-2 mx-md-0 px-md-0 mb-4 mb-md-5 module">Platform Storytelling</h1>
+            <h2 class="home-oracle-heading-secondary mx-4 px-2 mx-md-0 px-md-0 mb-2 mb-md-4 module">Helping a software giant persuade with compelling stories of transformation</h2>
+            <a class="home-oracle-link mx-3 px-2 mx-md-0 px-md-0 module" href="#" id="oracle-video-play-link"><i class="material-icons">play_arrow</i> Play the Video</a>
         </div>
     </div>
 </div>
@@ -130,13 +130,13 @@
 <div class="container-fluid my-4 py-4 my-lg-3 py-lg-3 home-eagle-container">
     <div class="row home-eagle-row">
         <div class="col-lg-8 order-lg-2 pl-lg-0">
-            <img class="img-fluid home-eagle-image" src="/images/home-eagle-image.png" alt="Eagle image">
+            <img class="img-fluid home-eagle-image module" src="/images/home-eagle-image.png" alt="Eagle image">
         </div>
 
         <div class="col-lg-4 order-lg-1 px-lg-0 col-xl-3 offset-xl-1 align-self-center">
-            <h1 class="home-eagle-heading-primary mx-4 px-2 mx-md-0 px-md-0 mb-4 mb-md-5">Accelerating Data Driven Discovery</h1>
-            <h2 class="home-eagle-heading-secondary mx-4 px-2 mx-md-0 px-md-0 mb-2 mb-md-4">Visualization based AI to power genomic research and insights</h2>
-            <a class="home-eagle-link mx-4 px-2 mx-md-0 px-md-0" href="#">+ Read the Case Study</a>
+            <h1 class="home-eagle-heading-primary mx-4 px-2 mx-md-0 px-md-0 mb-4 mb-md-5 module">Accelerating Data Driven Discovery</h1>
+            <h2 class="home-eagle-heading-secondary mx-4 px-2 mx-md-0 px-md-0 mb-2 mb-md-4 module">Visualization based AI to power genomic research and insights</h2>
+            <a class="home-eagle-link mx-4 px-2 mx-md-0 px-md-0 module" href="#">+ Read the Case Study</a>
         </div>
     </div>
 </div>
@@ -145,7 +145,7 @@
 <!-- Amadeus -->
 <div class="container-fluid my-4 py-4 my-lg-0 py-lg-0">
     <div class="row home-amadeus-row">
-        <div class="col-12 col-lg-auto">
+        <div class="col-12 col-lg-auto module">
             <img class="home-amadeus-image d-block" src="/images/home-amadeus-image.png" alt="Amadeus image">
 
             <div class="home-amadeus-video-wrapper m-auto">
@@ -164,16 +164,16 @@
         </div>
 
         <div class="col-12 col-lg-5 col-xl-3 px-xl-0 align-self-center">
-            <h1 class="home-amadeus-heading-primary mx-4 px-2 mx-md-0 px-md-0 mb-4 mb-md-5">Best Laid Plans</h1>
-            <h2 class="home-amadeus-heading-secondary mx-4 px-2 mx-md-0 px-md-0 mb-2 mb-md-4">Designing a cohesive SaaS product suite for professional event planners</h2>
-            <a class="home-amadeus-link mx-4 px-2 mx-md-0 px-md-0" href="#">+ Read the Case Study</a>
+            <h1 class="home-amadeus-heading-primary mx-4 px-2 mx-md-0 px-md-0 mb-4 mb-md-5 module">Best Laid Plans</h1>
+            <h2 class="home-amadeus-heading-secondary mx-4 px-2 mx-md-0 px-md-0 mb-2 mb-md-4 module">Designing a cohesive SaaS product suite for professional event planners</h2>
+            <a class="home-amadeus-link mx-4 px-2 mx-md-0 px-md-0 module" href="#">+ Read the Case Study</a>
         </div>
     </div>
 </div>
 
 
 <!-- More Work -->
-<div class="container my-5 py-5">
+<div class="container my-5 py-5 module">
     <div class="row mb-5">
         <div class="col text-center">
             <a href="#" class="home-more-work-button px-5 py-3">+ More Work</a>
@@ -183,60 +183,60 @@
 
 
 <!-- Clients -->
-<div class="container-fluid text-center mt-5 py-5 home-clients">
+<div class="container-fluid text-center mt-5 py-5 home-clients module">
     <div class="row d-none d-md-block">
         <div class="col">
-            <h3 class="my-5 py-5 home-clients-heading">Our Clients</h3>
+            <h3 class="my-5 py-5 home-clients-heading module">Our Clients</h3>
         </div>
     </div>
 
     <div class="row align-items-center mb-5">
         <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 offset-xl-1 my-4 py-4">
-            <img class="home-clients-logo-oracle" src="/images/logo-oracle.svg" alt="Oracle logo">
+            <img class="home-clients-logo-oracle module" src="/images/logo-oracle.svg" alt="Oracle logo">
         </div>
 
         <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 my-4 py-4">
-            <img class="home-clients-logo-adobe" src="/images/logo-adobe.svg" alt="Adobe logo">
+            <img class="home-clients-logo-adobe module" src="/images/logo-adobe.svg" alt="Adobe logo">
         </div>
 
         <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 my-4 py-4">
-            <img class="home-clients-logo-philips" src="/images/logo-philips.svg" alt="Philips logo">
+            <img class="home-clients-logo-philips module" src="/images/logo-philips.svg" alt="Philips logo">
         </div>
 
         <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 my-4 py-4">
-            <img class="home-clients-logo-frustum" src="/images/logo-frustum.png" alt="Frustum logo">
+            <img class="home-clients-logo-frustum module" src="/images/logo-frustum.png" alt="Frustum logo">
         </div>
 
         <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 my-4 py-4">
-            <img class="home-clients-logo-citrix" src="/images/logo-citrix.svg" alt="Citrix logo">
+            <img class="home-clients-logo-citrix module" src="/images/logo-citrix.svg" alt="Citrix logo">
         </div>
 
         <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 offset-xl-2 my-4 py-4">
-            <img class="home-clients-logo-zynga" src="/images/logo-zynga.svg" alt="Zynga logo">
+            <img class="home-clients-logo-zynga module" src="/images/logo-zynga.svg" alt="Zynga logo">
         </div>
 
         <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 my-4 py-4">
-            <img class="home-clients-logo-solidworks" src="/images/logo-solidworks.svg" alt="Solidworks logo">
+            <img class="home-clients-logo-solidworks module" src="/images/logo-solidworks.svg" alt="Solidworks logo">
         </div>
 
         <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 my-4 py-4">
-            <img class="home-clients-logo-scribe" src="/images/logo-scribe.png" alt="Scribe logo">
+            <img class="home-clients-logo-scribe module" src="/images/logo-scribe.png" alt="Scribe logo">
         </div>
 
         <div class="col-12 col-md-4 col-lg-12 col-xl-2 my-4 py-4">
-            <img class="home-clients-logo-eagle" src="/images/logo-eagle.svg" alt="Eagle logo">
+            <img class="home-clients-logo-eagle module" src="/images/logo-eagle.svg" alt="Eagle logo">
         </div>
     </div>
 </div>
 
 
 <!-- Testimonials -->
-<div id="home-carousel-testimonial" class="carousel carousel-fade" data-ride="carousel" data-interval="10000">
+<div id="home-carousel-testimonial" class="carousel carousel-fade module" data-ride="carousel" data-interval="10000">
     <div class="carousel-inner">
         <div class="carousel-item home-carousel-testimonial-item home-carousel-testimonial-item-adobe active">
             <div class="carousel-caption home-carousel-testimonial-caption">
-                <img class="home-carousel-testimonial-logo-adobe" src="/images/logo-adobe.svg" alt="Adobe logo">
-                <p class="home-carousel-testimonial-text">“As Adobe has grown and expanded its product lines over the years, we have relied on the outside expertise of Proximity Lab to help ensure we are designing and developing the most usable and effective tools possible for the creative marketplace. In many cases, they are not only a trusted partner, but serve as an extension to our design team to help on everything from product-focused UI exercises to executive level vision work.”</p>
+                <img class="home-carousel-testimonial-logo-adobe module-fade" src="/images/logo-adobe.svg" alt="Adobe logo">
+                <p class="home-carousel-testimonial-text module-fade">“As Adobe has grown and expanded its product lines over the years, we have relied on the outside expertise of Proximity Lab to help ensure we are designing and developing the most usable and effective tools possible for the creative marketplace. In many cases, they are not only a trusted partner, but serve as an extension to our design team to help on everything from product-focused UI exercises to executive level vision work.”</p>
                 <img class="home-carousel-testimonial-author" src="/images/profile-jamie-myrold-2x.png" alt="Testimonial author">
                 <p class="home-carousel-testimonial-author-title"><b>Jamie Myrold</b><br>VP of Design Adobe</p>
             </div>
@@ -244,8 +244,8 @@
 
         <div class="carousel-item home-carousel-testimonial-item home-carousel-testimonial-item-scribe">
             <div class="carousel-caption home-carousel-testimonial-caption">
-                <img class="home-carousel-testimonial-logo-scribe" src="/images/logo-scribe.png" alt="Scribe logo">
-                <p class="home-carousel-testimonial-text">“Proximity Lab understands our business - they challenged our thinking to help create new paradigms for data integration. The resulting user-centric design is now driving the product roadmap for our SaaS platform - we are seeing a new category of users performing data integration tasks that would have previously been outside of their skillset.”</p>
+                <img class="home-carousel-testimonial-logo-scribe module-fade" src="/images/logo-scribe.png" alt="Scribe logo">
+                <p class="home-carousel-testimonial-text module-fade">“Proximity Lab understands our business - they challenged our thinking to help create new paradigms for data integration. The resulting user-centric design is now driving the product roadmap for our SaaS platform - we are seeing a new category of users performing data integration tasks that would have previously been outside of their skillset.”</p>
                 <img class="home-carousel-testimonial-author" src="/images/testimonial-author-scribe-2x.png" alt="Testimonial author">
                 <p class="home-carousel-testimonial-author-title"><b>Shawn McGowan</b><br>CEO Scribe</p>
             </div>
@@ -253,8 +253,8 @@
 
         <div class="carousel-item home-carousel-testimonial-item home-carousel-testimonial-item-eagle">
             <div class="carousel-caption home-carousel-testimonial-caption">
-                <img class="home-carousel-testimonial-logo-eagle" src="/images/logo-eagle.svg" alt="Eagle logo">
-                <p class="home-carousel-testimonial-text">“Proximity Lab was an excellent UX/UI product strategy and design partner. They helped us accelerate our understanding of user journeys and user profiles, and ultimately created stunning visual designs that enabled us to demonstrate the power of our product to current and new customers.”</p>
+                <img class="home-carousel-testimonial-logo-eagle module-fade" src="/images/logo-eagle.svg" alt="Eagle logo">
+                <p class="home-carousel-testimonial-text module-fade">“Proximity Lab was an excellent UX/UI product strategy and design partner. They helped us accelerate our understanding of user journeys and user profiles, and ultimately created stunning visual designs that enabled us to demonstrate the power of our product to current and new customers.”</p>
                 <img class="home-carousel-testimonial-author" src="/images/profile_ureta_vidal_2x.png" alt="Testimonial author">
                 <p class="home-carousel-testimonial-author-title"><b>Abel Ureta-Vidal</b><br>CEO Eagle Genomics</p>
             </div>
@@ -262,8 +262,8 @@
 
         <div class="carousel-item home-carousel-testimonial-item home-carousel-testimonial-item-minim">
             <div class="carousel-caption home-carousel-testimonial-caption">
-                <img class="home-carousel-testimonial-logo-minim" src="/images/logo-minim-white-169x60.png" alt="Minim logo">
-                <p class="home-carousel-testimonial-text">“Proximity Lab is at the top of the game for user experience and design. Having worked with them on multiple occasions and projects, they have proven to be a firm that goes way beyond producing simple mock-ups. They take the time to understand the core solution a product needs to deliver.”</p>
+                <img class="home-carousel-testimonial-logo-minim module-fade" src="/images/logo-minim-white-169x60.png" alt="Minim logo">
+                <p class="home-carousel-testimonial-text module-fade">“Proximity Lab is at the top of the game for user experience and design. Having worked with them on multiple occasions and projects, they have proven to be a firm that goes way beyond producing simple mock-ups. They take the time to understand the core solution a product needs to deliver.”</p>
                 <img class="home-carousel-testimonial-author" src="/images/profile-jeremy-hitchcock-2x.png" alt="Testimonial author">
                 <p class="home-carousel-testimonial-author-title"><b>Jeremy Hitchcock</b><br>Founder &amp; COO Minim</p>
             </div>
