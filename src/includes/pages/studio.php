e2e0786c93c810a238e3fdee43c941a05ffa3869
Nuno Arruda <nuno@nunoarruda.com>
2018-07-17 14:57:50 +0000

    feat: add studio copy

--- src/includes/pages/studio.php
+++ src/includes/pages/studio.php
@@ -1,1 +1,9 @@
-test
+<!-- Studio info -->
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1 class="studio-heading mb-5">Strategic Research &amp; UX Design Studio</h1>
+            <p class="studio-copy">Proximity Lab is an award-winning interaction design studio with deep experienced in research, product strategy and UX design. We are designers, creators and thinkers who bring diverse backgrounds together to develop a common vision – to create products that emphasize clarity, simplicity and value.</p>
+        </div>
+    </div>
+</div>
