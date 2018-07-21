777f5393358f6d68784f281f538ed93f1ad16fe5
Nuno Arruda <nuno@nunoarruda.com>
2018-07-21 20:54:53 +0000

    feat: add Eric info

--- src/includes/pages/studio.php
+++ src/includes/pages/studio.php
@@ -118,7 +118,26 @@
             </div>
         </div>
 
-        <div class="col-12 col-md-6 col-lg-4 mb-5"><img class="img-fluid" src="/images/team/eric_2x.jpg" alt="Eric photo"></div>
+        <div class="col-12 col-md-6 col-lg-4 mb-5">
+            <div class="studio-team-person">
+                <img class="img-fluid" src="/images/team/eric_2x.jpg" alt="Eric photo">
+                <div class="studio-team-overlay">
+                    <div class="studio-team-person-wrapper">
+                        <div class="studio-team-person-detail-wrapper">
+                            <h3 class="studio-team-person-name">Eric ???</h3>
+                            <h4 class="studio-team-person-title">???</h3>
+                        </div>
+
+                        <div class="studio-team-person-linkedin-wrapper text-center">
+                            <a class="studio-team-person-linkedin" href="???" target="_blank"><img src="/images/icon-linkedin.svg"></a>
+                        </div>
+
+                        <hr class="studio-team-person-rule">
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <div class="col-12 col-md-6 col-lg-4 mb-5"><img class="img-fluid" src="/images/team/kim_2x.jpg" alt="Kim photo"></div>
         <div class="col-12 col-md-6 col-lg-4 mb-5"><img class="img-fluid" src="/images/team/rusty_2x.jpg" alt="Rusty photo"></div>
     </div>
