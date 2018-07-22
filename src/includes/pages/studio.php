1490711750e9b25cb2d1634962fb46902c543807
Nuno Arruda <nuno@nunoarruda.com>
2018-07-22 14:35:21 +0000

    refactor: spacing adjustments on mobile

--- src/includes/pages/studio.php
+++ src/includes/pages/studio.php
@@ -2,177 +2,181 @@
 <div class="container studio-container">
     <div class="row">
         <div class="col">
-            <h1 class="studio-heading mb-3 mb-sm-5">Strategic Research &amp; UX Design Studio</h1>
-            <p class="studio-copy">Proximity Lab is an award-winning interaction design studio with deep experienced in research, product strategy and UX design. We are designers, creators and thinkers who bring diverse backgrounds together to develop a common vision – to create products that emphasize clarity, simplicity and value.</p>
-            <img class="img-fluid mt-3 pt-3 mt-md-5 pt-md-5" src="/images/flow-diagram.png" alt="Flow diagram">
+            <div class="mx-4 mx-md-0">
+                <h1 class="studio-heading mb-3 mb-sm-5">Strategic Research &amp; UX Design Studio</h1>
+                <p class="studio-copy">Proximity Lab is an award-winning interaction design studio with deep experienced in research, product strategy and UX design. We are designers, creators and thinkers who bring diverse backgrounds together to develop a common vision – to create products that emphasize clarity, simplicity and value.</p>
+                <img class="img-fluid mt-3 pt-3 mt-md-5 pt-md-5" src="/images/flow-diagram.png" alt="Flow diagram">
+            </div>
         </div>
     </div>
 </div>
 
 <!-- Team -->
 <div class="container studio-team-container">
-    <div class="row">
-        <div class="col">
-            <h2 class="studio-team-heading mb-3 mb-sm-5 pb-4">Our Team</h2>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-12 col-md-6 col-lg-4 mb-5">
-            <div class="studio-team-person">
-                <img class="img-fluid" src="/images/team/evan_2x.jpg" alt="Evan photo">
-                <div class="studio-team-overlay">
-                    <div class="studio-team-person-wrapper">
-                        <div class="studio-team-person-detail-wrapper">
-                            <h3 class="studio-team-person-name">Evan Karatzas</h3>
-                            <h4 class="studio-team-person-title">Founder &amp; CEO</h3>
-                        </div>
-
-                        <div class="studio-team-person-linkedin-wrapper text-center">
-                            <a class="studio-team-person-linkedin" href="https://www.linkedin.com/in/ekaratzas/" target="_blank"><img src="/images/icon-linkedin.svg"></a>
-                        </div>
-
-                        <hr class="studio-team-person-rule">
-                    </div>
-                </div>
+    <div class="mx-4 mx-md-0">
+        <div class="row">
+            <div class="col">
+                <h2 class="studio-team-heading mb-3 mb-sm-5 pb-4">Our Team</h2>
             </div>
         </div>
 
-        <div class="col-12 col-md-6 col-lg-4 mb-5">
-            <div class="studio-team-person">
-                <img class="img-fluid" src="/images/team/nick_2x.jpg" alt="Nick photo">
-                <div class="studio-team-overlay">
-                    <div class="studio-team-person-wrapper">
-                        <div class="studio-team-person-detail-wrapper">
-                            <h3 class="studio-team-person-name">Nick Allen</h3>
-                            <h4 class="studio-team-person-title">Dir. of Client Strategy &amp; Research</h3>
-                        </div>
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-4 mb-5">
+                <div class="studio-team-person">
+                    <img class="img-fluid" src="/images/team/evan_2x.jpg" alt="Evan photo">
+                    <div class="studio-team-overlay">
+                        <div class="studio-team-person-wrapper">
+                            <div class="studio-team-person-detail-wrapper">
+                                <h3 class="studio-team-person-name">Evan Karatzas</h3>
+                                <h4 class="studio-team-person-title">Founder &amp; CEO</h3>
+                            </div>
 
-                        <div class="studio-team-person-linkedin-wrapper text-center">
-                            <a class="studio-team-person-linkedin" href="https://www.linkedin.com/in/nickallen/" target="_blank"><img src="/images/icon-linkedin.svg"></a>
-                        </div>
+                            <div class="studio-team-person-linkedin-wrapper text-center">
+                                <a class="studio-team-person-linkedin" href="https://www.linkedin.com/in/ekaratzas/" target="_blank"><img src="/images/icon-linkedin.svg"></a>
+                            </div>
 
-                        <hr class="studio-team-person-rule">
+                            <hr class="studio-team-person-rule">
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="col-12 col-md-6 col-lg-4 mb-5">
-            <div class="studio-team-person">
-                <img class="img-fluid" src="/images/team/ryan_2x.jpg" alt="Ryan photo">
-                <div class="studio-team-overlay">
-                    <div class="studio-team-person-wrapper">
-                        <div class="studio-team-person-detail-wrapper">
-                            <h3 class="studio-team-person-name">Ryan Szepan</h3>
-                            <h4 class="studio-team-person-title">Senior UX Designer</h3>
+            <div class="col-12 col-md-6 col-lg-4 mb-5">
+                <div class="studio-team-person">
+                    <img class="img-fluid" src="/images/team/nick_2x.jpg" alt="Nick photo">
+                    <div class="studio-team-overlay">
+                        <div class="studio-team-person-wrapper">
+                            <div class="studio-team-person-detail-wrapper">
+                                <h3 class="studio-team-person-name">Nick Allen</h3>
+                                <h4 class="studio-team-person-title">Dir. of Client Strategy &amp; Research</h3>
+                            </div>
+
+                            <div class="studio-team-person-linkedin-wrapper text-center">
+                                <a class="studio-team-person-linkedin" href="https://www.linkedin.com/in/nickallen/" target="_blank"><img src="/images/icon-linkedin.svg"></a>
+                            </div>
+
+                            <hr class="studio-team-person-rule">
                         </div>
-
-                        <div class="studio-team-person-linkedin-wrapper text-center">
-                            <a class="studio-team-person-linkedin" href="https://www.linkedin.com/in/szepan/" target="_blank"><img src="/images/icon-linkedin.svg"></a>
-                        </div>
-
-                        <hr class="studio-team-person-rule">
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="col-12 col-md-6 col-lg-4 mb-5">
-            <div class="studio-team-person">
-                <img class="img-fluid" src="/images/team/tom_2x.jpg" alt="Tom photo">
-                <div class="studio-team-overlay">
-                    <div class="studio-team-person-wrapper">
-                        <div class="studio-team-person-detail-wrapper">
-                            <h3 class="studio-team-person-name">Tom Rhinelander</h3>
-                            <h4 class="studio-team-person-title">SENIOR RESEARCHER</h3>
+            <div class="col-12 col-md-6 col-lg-4 mb-5">
+                <div class="studio-team-person">
+                    <img class="img-fluid" src="/images/team/ryan_2x.jpg" alt="Ryan photo">
+                    <div class="studio-team-overlay">
+                        <div class="studio-team-person-wrapper">
+                            <div class="studio-team-person-detail-wrapper">
+                                <h3 class="studio-team-person-name">Ryan Szepan</h3>
+                                <h4 class="studio-team-person-title">Senior UX Designer</h3>
+                            </div>
+
+                            <div class="studio-team-person-linkedin-wrapper text-center">
+                                <a class="studio-team-person-linkedin" href="https://www.linkedin.com/in/szepan/" target="_blank"><img src="/images/icon-linkedin.svg"></a>
+                            </div>
+
+                            <hr class="studio-team-person-rule">
                         </div>
-
-                        <div class="studio-team-person-linkedin-wrapper text-center">
-                            <a class="studio-team-person-linkedin" href="https://www.linkedin.com/in/rhinelander/" target="_blank"><img src="/images/icon-linkedin.svg"></a>
-                        </div>
-
-                        <hr class="studio-team-person-rule">
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="col-12 col-md-6 col-lg-4 mb-5">
-            <div class="studio-team-person">
-                <img class="img-fluid" src="/images/team/brian_2x.jpg" alt="Brian photo">
-                <div class="studio-team-overlay">
-                    <div class="studio-team-person-wrapper">
-                        <div class="studio-team-person-detail-wrapper">
-                            <h3 class="studio-team-person-name">Brian Lucid</h3>
-                            <h4 class="studio-team-person-title">SENIOR UX Designer</h3>
+            <div class="col-12 col-md-6 col-lg-4 mb-5">
+                <div class="studio-team-person">
+                    <img class="img-fluid" src="/images/team/tom_2x.jpg" alt="Tom photo">
+                    <div class="studio-team-overlay">
+                        <div class="studio-team-person-wrapper">
+                            <div class="studio-team-person-detail-wrapper">
+                                <h3 class="studio-team-person-name">Tom Rhinelander</h3>
+                                <h4 class="studio-team-person-title">SENIOR RESEARCHER</h3>
+                            </div>
+
+                            <div class="studio-team-person-linkedin-wrapper text-center">
+                                <a class="studio-team-person-linkedin" href="https://www.linkedin.com/in/rhinelander/" target="_blank"><img src="/images/icon-linkedin.svg"></a>
+                            </div>
+
+                            <hr class="studio-team-person-rule">
                         </div>
-
-                        <div class="studio-team-person-linkedin-wrapper text-center">
-                            <a class="studio-team-person-linkedin" href="https://www.linkedin.com/in/brianlucid/" target="_blank"><img src="/images/icon-linkedin.svg"></a>
-                        </div>
-
-                        <hr class="studio-team-person-rule">
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="col-12 col-md-6 col-lg-4 mb-5">
-            <div class="studio-team-person">
-                <img class="img-fluid" src="/images/team/eric_2x.jpg" alt="Eric photo">
-                <div class="studio-team-overlay">
-                    <div class="studio-team-person-wrapper">
-                        <div class="studio-team-person-detail-wrapper">
-                            <h3 class="studio-team-person-name">Eric ???</h3>
-                            <h4 class="studio-team-person-title">???</h3>
+            <div class="col-12 col-md-6 col-lg-4 mb-5">
+                <div class="studio-team-person">
+                    <img class="img-fluid" src="/images/team/brian_2x.jpg" alt="Brian photo">
+                    <div class="studio-team-overlay">
+                        <div class="studio-team-person-wrapper">
+                            <div class="studio-team-person-detail-wrapper">
+                                <h3 class="studio-team-person-name">Brian Lucid</h3>
+                                <h4 class="studio-team-person-title">SENIOR UX Designer</h3>
+                            </div>
+
+                            <div class="studio-team-person-linkedin-wrapper text-center">
+                                <a class="studio-team-person-linkedin" href="https://www.linkedin.com/in/brianlucid/" target="_blank"><img src="/images/icon-linkedin.svg"></a>
+                            </div>
+
+                            <hr class="studio-team-person-rule">
                         </div>
-
-                        <div class="studio-team-person-linkedin-wrapper text-center">
-                            <a class="studio-team-person-linkedin" href="???" target="_blank"><img src="/images/icon-linkedin.svg"></a>
-                        </div>
-
-                        <hr class="studio-team-person-rule">
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="col-12 col-md-6 col-lg-4 mb-5">
-            <div class="studio-team-person">
-                <img class="img-fluid" src="/images/team/kim_2x.jpg" alt="Kim photo">
-                <div class="studio-team-overlay">
-                    <div class="studio-team-person-wrapper">
-                        <div class="studio-team-person-detail-wrapper">
-                            <h3 class="studio-team-person-name">Kim ???</h3>
-                            <h4 class="studio-team-person-title">???</h3>
+            <div class="col-12 col-md-6 col-lg-4 mb-5">
+                <div class="studio-team-person">
+                    <img class="img-fluid" src="/images/team/eric_2x.jpg" alt="Eric photo">
+                    <div class="studio-team-overlay">
+                        <div class="studio-team-person-wrapper">
+                            <div class="studio-team-person-detail-wrapper">
+                                <h3 class="studio-team-person-name">Eric ???</h3>
+                                <h4 class="studio-team-person-title">???</h3>
+                            </div>
+
+                            <div class="studio-team-person-linkedin-wrapper text-center">
+                                <a class="studio-team-person-linkedin" href="???" target="_blank"><img src="/images/icon-linkedin.svg"></a>
+                            </div>
+
+                            <hr class="studio-team-person-rule">
                         </div>
-
-                        <div class="studio-team-person-linkedin-wrapper text-center">
-                            <a class="studio-team-person-linkedin" href="???" target="_blank"><img src="/images/icon-linkedin.svg"></a>
-                        </div>
-
-                        <hr class="studio-team-person-rule">
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="col-12 col-md-6 col-lg-4 mb-5">
-            <div class="studio-team-person">
-                <img class="img-fluid" src="/images/team/rusty_2x.jpg" alt="Rusty photo">
-                <div class="studio-team-overlay">
-                    <div class="studio-team-person-wrapper">
-                        <div class="studio-team-person-detail-wrapper">
-                            <h3 class="studio-team-person-name">Rusty ???</h3>
-                            <h4 class="studio-team-person-title">???</h3>
+            <div class="col-12 col-md-6 col-lg-4 mb-5">
+                <div class="studio-team-person">
+                    <img class="img-fluid" src="/images/team/kim_2x.jpg" alt="Kim photo">
+                    <div class="studio-team-overlay">
+                        <div class="studio-team-person-wrapper">
+                            <div class="studio-team-person-detail-wrapper">
+                                <h3 class="studio-team-person-name">Kim ???</h3>
+                                <h4 class="studio-team-person-title">???</h3>
+                            </div>
+
+                            <div class="studio-team-person-linkedin-wrapper text-center">
+                                <a class="studio-team-person-linkedin" href="???" target="_blank"><img src="/images/icon-linkedin.svg"></a>
+                            </div>
+
+                            <hr class="studio-team-person-rule">
                         </div>
+                    </div>
+                </div>
+            </div>
 
-                        <div class="studio-team-person-linkedin-wrapper text-center">
-                            <a class="studio-team-person-linkedin" href="???" target="_blank"><img src="/images/icon-linkedin.svg"></a>
+            <div class="col-12 col-md-6 col-lg-4 mb-5">
+                <div class="studio-team-person">
+                    <img class="img-fluid" src="/images/team/rusty_2x.jpg" alt="Rusty photo">
+                    <div class="studio-team-overlay">
+                        <div class="studio-team-person-wrapper">
+                            <div class="studio-team-person-detail-wrapper">
+                                <h3 class="studio-team-person-name">Rusty ???</h3>
+                                <h4 class="studio-team-person-title">???</h3>
+                            </div>
+
+                            <div class="studio-team-person-linkedin-wrapper text-center">
+                                <a class="studio-team-person-linkedin" href="???" target="_blank"><img src="/images/icon-linkedin.svg"></a>
+                            </div>
+
+                            <hr class="studio-team-person-rule">
                         </div>
-
-                        <hr class="studio-team-person-rule">
                     </div>
                 </div>
             </div>
