f09b35a7930339393bd1243ab5feb7bca9a11ee1
Nuno Arruda <nuno@nunoarruda.com>
2018-07-22 14:01:30 +0000

    refactor: style adjustments to Studio text

--- src/includes/pages/studio.php
+++ src/includes/pages/studio.php
@@ -2,7 +2,7 @@
 <div class="container studio-container">
     <div class="row">
         <div class="col">
-            <h1 class="studio-heading mb-5">Strategic Research &amp; UX Design Studio</h1>
+            <h1 class="studio-heading mb-3 mb-sm-5">Strategic Research &amp; UX Design Studio</h1>
             <p class="studio-copy">Proximity Lab is an award-winning interaction design studio with deep experienced in research, product strategy and UX design. We are designers, creators and thinkers who bring diverse backgrounds together to develop a common vision – to create products that emphasize clarity, simplicity and value.</p>
             <img class="img-fluid mt-3 pt-3 mt-md-5 pt-md-5" src="/images/flow-diagram.png" alt="Flow diagram">
         </div>
