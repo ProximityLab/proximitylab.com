fd6391691f7f443a888328415525d7aa07a0141e
Nuno Arruda <nuno@nunoarruda.com>
2018-07-26 10:55:35 +0000

    feat: logo should link to homepage

--- src/includes/navigation-new.php
+++ src/includes/navigation-new.php
@@ -2,8 +2,10 @@
 <div class="container mt-5 mt-sm-5 pt-sm-4 module-fade">
     <div class="row">
         <div class="col-3 col-md-5">
-            <img src="/images/logo-proximity-lab-2018.svg" class="newnav-proximity-lab-logo d-md-none ml-4" alt="Proximity Lab logo">
-            <img src="/images/logo-and-name-proximity-lab-2018.svg" class="newnav-proximity-lab-logo d-none d-md-inline" alt="Proximity Lab logo">
+            <a href="/">
+                <img src="/images/logo-proximity-lab-2018.svg" class="newnav-proximity-lab-logo d-md-none ml-4" alt="Proximity Lab logo">
+                <img src="/images/logo-and-name-proximity-lab-2018.svg" class="newnav-proximity-lab-logo d-none d-md-inline" alt="Proximity Lab logo">
+            </a>
         </div>
 
         <div class="col-9 col-md-7 text-right">
