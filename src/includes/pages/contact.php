cb6202dfa8a609ece94b6669016c138b353d109d
Nuno Arruda <nuno@nunoarruda.com>
2018-08-11 21:23:15 +0000

    feat: add San Francisco info

--- src/includes/pages/contact.php
+++ src/includes/pages/contact.php
@@ -40,10 +40,10 @@
 
 
 <!-- Portsmouth -->
-<div class="container mt-5 pt-md-5">
+<div class="container mt-5 pt-2 pt-md-5">
     <div class="row mt-5 px-4 px-md-0">
         <div class="col-12">
-            <h1 class="contact-heading mb-5">Portsmouth</h1>
+            <h1 class="contact-heading mb-4">Portsmouth</h1>
         </div>
 
         <div class="col-12 col-sm mb-4"><img class="img-fluid" src="/images/contact-photo-portsmouth1.jpg" alt="Portsmouth photo"></div>
@@ -52,6 +52,25 @@
             <p class="contact-office-info">
                 <img class="contact-icon-location" src="/images/contact-icon-location.svg" alt="Location icon">
                 3 Congress Street   Portsmouth, NH  03801
+            </p>
+        </div>
+    </div>
+</div>
+
+
+<!-- San Francisco -->
+<div class="container mt-5 pt-md-5">
+    <div class="row mt-5 px-4 px-md-0">
+        <div class="col-12">
+            <h1 class="contact-heading mb-4">San Francisco</h1>
+        </div>
+
+        <div class="col-12 col-sm mb-4"><img class="img-fluid" src="/images/contact-photo-sanfrancisco1.jpg" alt="San Francisco photo"></div>
+        <div class="col-12 col-sm mb-4"><img class="img-fluid" src="/images/contact-photo-sanfrancisco2.jpg" alt="San Francisco photo"></div>
+        <div class="col-12">
+            <p class="contact-office-info">
+                <img class="contact-icon-location" src="/images/contact-icon-location.svg" alt="Location icon">
+                1161 Mission Street   San Francisco, CA  94103
             </p>
         </div>
     </div>
