705e82018b6a2dc9a81b89471240c9173d385d22
Nuno Arruda <nuno@nunoarruda.com>
2018-08-15 15:10:43 +0000

    refactor: reduce the scale of the contact form

--- src/includes/pages/contact.php
+++ src/includes/pages/contact.php
@@ -8,28 +8,36 @@
 
     <form class="px-4 px-md-0 module" action="https://formspree.io/info@proximitylab.com" method="POST">
         <div class="form-row">
-            <div class="col-12 col-md-6 col-xl-4 mb-4">
-                <input type="text" class="form-control form-control-lg contact-input" placeholder="First name" name="firstName" required>
+            <div class="col-12 col-md-6 col-xl-3 mb-4">
+                <input type="text" class="form-control contact-input" placeholder="First name" name="firstName" required>
             </div>
 
-            <div class="col-12 col-md-6 col-xl-4 mb-4">
-                <input type="text" class="form-control form-control-lg contact-input" placeholder="Last name" name="lastName">
+            <div class="col-12 col-md-6 col-xl-3 mb-4">
+                <input type="text" class="form-control contact-input" placeholder="Last name" name="lastName">
             </div>
 
-            <div class="col-12 col-xl-8 mb-4">
-                <input type="text" class="form-control form-control-lg contact-input" placeholder="Company" name="company">
+            <div class="w-100"></div>
+
+            <div class="col-12 col-xl-6 mb-4">
+                <input type="text" class="form-control contact-input" placeholder="Company" name="company">
             </div>
 
-            <div class="col-12 col-md-6 col-xl-8 mb-4">
-                <input type="email" class="form-control form-control-lg contact-input" placeholder="Email" name="_replyto" required>
+            <div class="w-100"></div>
+
+            <div class="col-12 col-md-6 mb-4">
+                <input type="email" class="form-control contact-input" placeholder="Email" name="_replyto" required>
             </div>
 
-            <div class="col-12 col-md-6 col-xl-8 mb-4">
-                <input type="tel" class="form-control form-control-lg contact-input" placeholder="Phone" name="phone">
+            <div class="w-100 d-none d-xl-block"></div>
+
+            <div class="col-12 col-md-6 mb-4">
+                <input type="tel" class="form-control contact-input" placeholder="Phone" name="phone">
             </div>
 
-            <div class="col-12 col-xl-8 mb-5">
-                <textarea class="form-control form-control-lg contact-input" rows="5" placeholder="Comments" name="comments" required></textarea>
+            <div class="w-100"></div>
+
+            <div class="col-12 col-xl-6 mb-5">
+                <textarea class="form-control contact-input" rows="4" placeholder="Comments" name="comments" required></textarea>
                 <input type="text" name="_gotcha" style="display:none">
             </div>
         </div>
