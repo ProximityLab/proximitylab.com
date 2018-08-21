a677bd2ed5a0c7af83363edd6bc50edbeccf6754
Nuno Arruda <nuno@nunoarruda.com>
2018-08-21 22:12:08 +0000

    refactor: apply new contact page layout

--- src/includes/pages/contact.php
+++ src/includes/pages/contact.php
@@ -1,6 +1,6 @@
-<div class="container mt-5 pt-md-5">
+<div class="container mt-5 pt-md-5 module-fade">
     <div class="row px-4 px-md-0">
-        <div class="col-12 col-xl-4">
+        <div class="col-12 col-lg-4 module">
             <p class="contact-info mb-5">Proximity Lab has locations on both coasts to enable collaboration with your product teams.</p>
 
             <img class="contact-icon-location" src="/images/contact-icon-location.svg" alt="Location icon">
@@ -22,95 +22,20 @@
             <a class="contact-phone" href="tel:1-855-PROXLAB">1-855-PROXLAB</a>
         </div>
 
-        <div class="col-12 col-xl-6">
-            <h1 class="contact-heading">What’s on your mind?</h1>
+        <div class="col-12 col-lg-8 mt-5 mt-lg-0 module">
+            <h1 class="contact-heading mb-5">What’s on your mind?</h1>
+
+            <form class="module" action="https://formspree.io/info@proximitylab.com" method="POST">
+                <div class="row">
+                    <div class="col-12 col-md-6 mb-5"><input type="text" class="contact-input" placeholder="First Name" name="firstName" required></div>
+                    <div class="col-12 col-md-6 mb-5"><input type="text" class="contact-input" placeholder="Last name" name="lastName"></div>
+                    <div class="col-12 mb-5"><input type="text" class="contact-input" placeholder="Company" name="company"></div>
+                    <div class="col-12 mb-5"><input type="email" class="contact-input" placeholder="Email" name="_replyto" required></div>
+                    <div class="col-12 mb-5"><input type="tel" class="contact-input" placeholder="Phone" name="phone"></div>
+                    <div class="col-12 mb-4"><textarea class="contact-input" rows="4" placeholder="How can we help?" name="comments" required></textarea></div>
+                </div>
+                <button type="submit" class="btn btn-primary contact-button px-5">Submit</button>
+            </form>
         </div>
     </div>
 </div>
-
-
-<!-- Form -->
-<!-- <div class="container mt-5 pt-md-5 module-fade">
-    <div class="row mt-3 px-4 px-md-0 module">
-        <div class="col">
-            <h1 class="contact-heading mb-5">Talk to us.</h1>
-        </div>
-    </div>
-
-    <form class="px-4 px-md-0 module" action="https://formspree.io/info@proximitylab.com" method="POST">
-        <div class="form-row">
-            <div class="col-12 col-md-6 col-xl-3 mb-4">
-                <input type="text" class="form-control contact-input" placeholder="First name" name="firstName" required>
-            </div>
-
-            <div class="col-12 col-md-6 col-xl-3 mb-4">
-                <input type="text" class="form-control contact-input" placeholder="Last name" name="lastName">
-            </div>
-
-            <div class="w-100"></div>
-
-            <div class="col-12 col-xl-6 mb-4">
-                <input type="text" class="form-control contact-input" placeholder="Company" name="company">
-            </div>
-
-            <div class="w-100"></div>
-
-            <div class="col-12 col-md-6 mb-4">
-                <input type="email" class="form-control contact-input" placeholder="Email" name="_replyto" required>
-            </div>
-
-            <div class="w-100 d-none d-xl-block"></div>
-
-            <div class="col-12 col-md-6 mb-4">
-                <input type="tel" class="form-control contact-input" placeholder="Phone" name="phone">
-            </div>
-
-            <div class="w-100"></div>
-
-            <div class="col-12 col-xl-6 mb-5">
-                <textarea class="form-control contact-input" rows="4" placeholder="Comments" name="comments" required></textarea>
-                <input type="text" name="_gotcha" style="display:none">
-            </div>
-        </div>
-
-        <button type="submit" class="btn btn-primary contact-button px-5">Submit</button>
-    </form>
-</div> -->
-
-
-<!-- Portsmouth -->
-<!-- <div class="container mt-5 pt-2 pt-md-5 module-fade">
-    <div class="row mt-5 px-4 px-md-0 module">
-        <div class="col-12">
-            <h1 class="contact-heading mb-4">Portsmouth</h1>
-        </div>
-
-        <div class="col-12 col-sm col-xl-5 mb-4"><img class="img-fluid" src="/images/contact-photo-portsmouth1.jpg" alt="Portsmouth photo"></div>
-        <div class="col-12 col-sm col-xl-5 mb-4"><img class="img-fluid" src="/images/contact-photo-portsmouth2.jpg" alt="Portsmouth photo"></div>
-        <div class="col-12">
-            <p class="contact-office-info">
-                <img class="contact-icon-location" src="/images/contact-icon-location.svg" alt="Location icon">
-                3 Congress Street   Portsmouth, NH  03801
-            </p>
-        </div>
-    </div>
-</div> -->
-
-
-<!-- San Francisco -->
-<!-- <div class="container mt-5 pt-md-5 module-fade">
-    <div class="row mt-5 px-4 px-md-0 module">
-        <div class="col-12">
-            <h1 class="contact-heading mb-4">San Francisco</h1>
-        </div>
-
-        <div class="col-12 col-sm col-xl-5 mb-4"><img class="img-fluid" src="/images/contact-photo-sanfrancisco1.jpg" alt="San Francisco photo"></div>
-        <div class="col-12 col-sm col-xl-5 mb-4"><img class="img-fluid" src="/images/contact-photo-sanfrancisco2.jpg" alt="San Francisco photo"></div>
-        <div class="col-12">
-            <p class="contact-office-info">
-                <img class="contact-icon-location" src="/images/contact-icon-location.svg" alt="Location icon">
-                1161 Mission Street   San Francisco, CA  94103
-            </p>
-        </div>
-    </div>
-</div> -->
